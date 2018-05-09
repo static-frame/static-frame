6e11cc975045780c15e4a2b0a60ade900b06b15a
ariza <ariza@flexatone.com>
2018-05-08 20:46:36 -0700

    updated doc conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -107,7 +107,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+html_theme = 'default'
 
 # import sphinx_rtd_theme
 # html_theme = "sphinx_rtd_theme"
