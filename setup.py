66824779f4a12f9c6f8dbdd71d9ce7e603b93e69
flexatone <ariza@flexatone.com>
2019-02-14 13:16:29 -0800

    updated usage of LICENSE.txt, added setup.cfg

--- setup.py
+++ setup.py
@@ -17,7 +17,14 @@ root_dir_fp = path.abspath(path.dirname(__file__))
 def get_long_description():
 #     with open(path.join(root_dir_fp, 'README.rst'), encoding='utf-8') as f:
 #         return f.read()
-    return '''The StaticFrame library consists of the Series and Frame, immutable data structures for one- and two-dimensional calculations with self-aligning, labelled axis. StaticFrame offers an alternative to Pandas. While many interfaces for data extraction and manipulation are similar to Pandas, StaticFrame deviates from Pandas in many ways: all data is immutable, and all indices must be unique; all vector processing uses NumPy, and the full range of NumPy data types is preserved; the implementation is concise and lightweight; consistent naming and interfaces are used throughout; and flexible approaches to iteration and function application, with built-in options for parallelization, are provided.'''
+    return '''The StaticFrame library consists of the Series and Frame, immutable data structures for one- and two-dimensional calculations with self-aligning, labelled axis. StaticFrame offers an alternative to Pandas. While many interfaces for data extraction and manipulation are similar to Pandas, StaticFrame deviates from Pandas in many ways: all data is immutable, and all indices must be unique; all vector processing uses NumPy, and the full range of NumPy data types is preserved; the implementation is concise and lightweight; consistent naming and interfaces are used throughout; and flexible approaches to iteration and function application, with built-in options for parallelization, are provided.
+
+Code: https://github.com/InvestmentSystems/static-frame
+
+Docs: http://static-frame.readthedocs.io
+
+Packages: https://pypi.org/project/static-frame
+'''
 
 def get_version():
     with open(path.join(root_dir_fp, 'static_frame', '__init__.py'), encoding='utf-8') as f:
