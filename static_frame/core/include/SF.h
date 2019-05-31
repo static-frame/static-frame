f91bfee7677f3ba58e28c3ac21418b7730ad0b97
Brandt Bucher <brandtbucher@gmail.com>
2019-05-30 19:04:56 -0700

    Break out shared utilities into SF.c.

---
+++ static_frame/core/include/SF.h
@@ -0,0 +1,25 @@
+# ifndef SF_H
+# define SF_H
+
+# include "Python.h"
+# include "structmember.h"
+
+# define PY_ARRAY_UNIQUE_SYMBOL SF_ARRAY_API
+# define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
+# include "numpy/arrayobject.h"
+
+
+/* Bug in numpy < 1.16: https://github.com/numpy/numpy/pull/12131 */
+
+# undef PyDataType_ISBOOL
+# define PyDataType_ISBOOL(obj) PyTypeNum_ISBOOL(((PyArray_Descr*)(obj))->type_num)
+
+
+PyArrayObject* SFUtil_ImmutableFilter(PyArrayObject*);
+
+PyArray_Descr* SFUtil_ResolveDTypes(PyArray_Descr*, PyArray_Descr*);
+
+PyArray_Descr* SFUtil_ResolveDTypesIter(PyObject*);
+
+# endif
--- static_frame/core/include/util.h
+++ static_frame/core/include/util.h
@@ -1,23 +1,6 @@
 # ifndef UTIL_H
 # define UTIL_H
 
-# include "Python.h"
-
-# define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-
-# include "numpy/arrayobject.h"
-
-
-/* Until we require Numpy 1.16.0 or above: */
-
-# undef PyDataType_ISBOOL
-# define PyDataType_ISBOOL(obj) PyTypeNum_ISBOOL(((PyArray_Descr*)(obj))->type_num)
-
-
-PyArrayObject* SFUtil_ImmutableFilter(PyArrayObject*);
-
-PyArray_Descr* SFUtil_ResolveDTypes(PyArray_Descr*, PyArray_Descr*);
-
-PyArray_Descr* SFUtil_ResolveDTypesIter(PyObject*);
+# include "SF.h"
 
 # endif
