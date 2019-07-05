5e2ff36f4c4810a5abbf0600a87207bdc7419ceb
Brandt Bucher <brandtbucher@gmail.com>
2019-07-05 16:12:10 -0700

    Type test_type_blocks.

--- static_frame/test/property/test_type_blocks.py
+++ static_frame/test/property/test_type_blocks.py
@@ -1,13 +1,13 @@
 
-
+import typing as tp
 import unittest
 
-import numpy as np
+import numpy as np  # type: ignore
 
 from hypothesis import strategies as st
-from hypothesis import given
-from hypothesis import example
-from hypothesis import reproduce_failure
+from hypothesis import given  # type: ignore
+from hypothesis import example  # type: ignore
+from hypothesis import reproduce_failure  # type: ignore
 
 from static_frame.test.property import strategies as sfst
 
@@ -34,26 +34,26 @@ from static_frame import FrameGO
 class TestUnit(TestCase):
 
 
-    @given(sfst.get_array_1d2d())
-    def test_shape_filter(self, shape):
+    @given(sfst.get_array_1d2d())  # type: ignore
+    def test_shape_filter(self, shape: np.ndarray) -> None:
         self.assertTrue(len(TypeBlocks.shape_filter(shape)), 2)
 
 
-    @given(sfst.get_type_blocks())
-    def test_basic_attributes(self, tb):
+    @given(sfst.get_type_blocks())  # type: ignore
+    def test_basic_attributes(self, tb: TypeBlocks) -> None:
         self.assertTrue(len(tb.dtypes), len(tb))
         self.assertTrue(len(tb.shapes), len(tb.mloc))
 
 
-    @given(sfst.get_type_blocks())
-    def test_values(self, tb):
+    @given(sfst.get_type_blocks())  # type: ignore
+    def test_values(self, tb: TypeBlocks) -> None:
         values = tb.values
         self.assertEqual(values.shape, tb.shape)
         self.assertEqual(values.dtype, tb._row_dtype)
 
 
-    @given(sfst.get_type_blocks())
-    def test_element_items(self, tb):
+    @given(sfst.get_type_blocks())  # type: ignore
+    def test_element_items(self, tb: TypeBlocks) -> None:
 
         # NOTE: this found a flaw in _extract_iloc where we tried to optimize selection with a unified array
 
@@ -67,8 +67,8 @@ class TestUnit(TestCase):
         self.assertEqual(count, tb.size)
 
 
-    @given(sfst.get_type_blocks_aligned_array())
-    def test_append(self, tb_aligned_array):
+    @given(sfst.get_type_blocks_aligned_array())  # type: ignore
+    def test_append(self, tb_aligned_array: tp.Tuple[TypeBlocks, np.ndarray]) -> None:
         tb, aa = tb_aligned_array
         shape_original = tb.shape
         tb.append(aa)
@@ -77,13 +77,13 @@ class TestUnit(TestCase):
         else:
             self.assertEqual(tb.shape[1], shape_original[1] + aa.shape[1])
 
-    @given(sfst.get_type_blocks_aligned_type_blocks(min_size=2, max_size=2))
-    def test_extend(self, tbs):
+    @given(sfst.get_type_blocks_aligned_type_blocks(min_size=2, max_size=2))  # type: ignore
+    def test_extend(self, tbs: tp.List[TypeBlocks]) -> None:
         front = tbs[0]
         back = tbs[1]
         shape_original = front.shape
         # extend with type blocks
-        front.extend(back)
+        front.extend(back) 
         self.assertEqual(front.shape,
                 (shape_original[0], shape_original[1] + back.shape[1]))
 
