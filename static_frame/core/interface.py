8aa73ae1b98b36d7b1bfa68196e3c287ab94c49c
flexatone <ariza@flexatone.com>
2019-09-18 09:06:23 -0700

    added tests for InterfaceSummary

--- static_frame/core/interface.py
+++ static_frame/core/interface.py
@@ -32,98 +32,102 @@ class InterfaceGroup:
     Selector = 'selector'
 
 
-DOC_CHARS = 40
+class InterfaceSummary:
 
-EXCLUDE_PRIVATE = {
-    '__class__',
-    '__class_getitem__',
-    '__annotations__',
-    '__doc__',
-    '__delattr__',
-    '__dir__',
-    '__dict__',
-    '__format__',
-    '__getattribute__',
-    '__hash__',
-    '__init__',
-    '__init_sbclass__',
-    '__lshift__',
-    '__module__',
-    '__init_subclass__',
-    '__new__',
-    '__setattr__',
-    '__setstate__',
-    '__slots__',
-    '__subclasshook__',
-    '__weakref__',
-    '__reduce__',
-    '__reduce_ex__',
-    }
+    DOC_CHARS = 40
 
-DICT_LIKE = {'keys', 'values', 'items', '__contains__'}
+    EXCLUDE_PRIVATE = {
+        '__class__',
+        '__class_getitem__',
+        '__annotations__',
+        '__doc__',
+        '__delattr__',
+        '__dir__',
+        '__dict__',
+        '__format__',
+        '__getattribute__',
+        '__hash__',
+        '__init__',
+        '__init_sbclass__',
+        '__lshift__',
+        '__module__',
+        '__init_subclass__',
+        '__new__',
+        '__setattr__',
+        '__setstate__',
+        '__slots__',
+        '__subclasshook__',
+        '__weakref__',
+        '__reduce__',
+        '__reduce_ex__',
+        }
 
-ATTR_ITER_NODE = ('apply', 'apply_iter', 'apply_iter_items', 'apply_pool')
-ATTR_SELECTOR = ('__getitem__', 'iloc', 'loc', 'drop', 'mask', 'masked_array', 'assign')
+    DICT_LIKE = {'keys', 'values', 'items', '__contains__'}
+    ATTR_ITER_NODE = ('apply', 'apply_iter', 'apply_iter_items', 'apply_pool')
+    ATTR_SELECTOR = ('__getitem__', 'iloc', 'loc', 'drop', 'mask', 'masked_array', 'assign')
 
+    @classmethod
+    def is_public(cls, field: str) -> bool:
+        if field.startswith('_') and not field.startswith('__'):
+            return False
+        if field in cls.EXCLUDE_PRIVATE:
+            return False
+        return True
 
-def is_public(field: str) -> bool:
-    if field.startswith('_') and not field.startswith('__'):
-        return False
-    if field in EXCLUDE_PRIVATE:
-        return False
-    return True
+    @classmethod
+    def scrub_doc(cls, doc: tp.Optional[str]) -> str:
+        if not doc:
+            return ''
+        return doc.strip().replace('\n', ' ')[:cls.DOC_CHARS]
 
-def scrub_doc(doc: tp.Optional[str]) -> str:
-    if not doc:
-        return ''
-    return doc.strip().replace('\n', ' ')[:DOC_CHARS]
+    @classmethod
+    def interrogate(cls, target: tp.Any) -> tp.Iterator[Interface]:
 
-def interrogate(cls) -> tp.Iterator[Interface]:
+        for name_attr in sorted(dir(target)):
+            if not cls.is_public(name_attr):
+                continue
 
-    for name_attr in sorted(dir(cls)):
-        if not is_public(name_attr):
-            continue
+            # this gets object off the class, not an instance
+            obj = getattr(target, name_attr)
 
-        # this gets object off the class, not an instance
-        obj = getattr(cls, name_attr)
+            doc = ''
+            if hasattr(obj, '__doc__'):
+                doc = cls.scrub_doc(obj.__doc__)
 
-        doc = ''
-        if hasattr(obj, '__doc__'):
-            doc = scrub_doc(obj.__doc__)
-
-        if hasattr(obj, '__name__'):
-            name = obj.__name__
-        else:
-            name = name_attr
-
-        cls_name = cls.__name__
-
-
-        if name in DICT_LIKE:
-            yield Interface(cls_name, InterfaceGroup.DictLike, name, doc)
-        elif name.startswith('iter_'):
-            yield Interface(cls_name, InterfaceGroup.Iterator, name, doc)
-
-            for field in ATTR_ITER_NODE:
-                display = f'{name}(axis).{field}'
-                doc = scrub_doc(getattr(IterNodeDelegate, field).__doc__)
-                yield Interface(cls_name, InterfaceGroup.Iterator, display, doc)
-
-        elif callable(obj):
-            if name_attr in _UFUNC_UNARY_OPERATORS:
-                yield Interface(cls_name, InterfaceGroup.OperatorUnary, name, doc)
-            elif name_attr in _UFUNC_BINARY_OPERATORS or name_attr in _RIGHT_OPERATOR_MAP:
-                yield Interface(cls_name, InterfaceGroup.OperatorBinary, name, doc)
+            if hasattr(obj, '__name__'):
+                name = obj.__name__
             else:
-                yield Interface(cls_name, InterfaceGroup.Method, name, doc)
-        else:
-            yield Interface(cls_name, InterfaceGroup.Attribute, name, doc)
+                name = name_attr
 
+            cls_name = target.__name__
+
+
+            if name in cls.DICT_LIKE:
+                yield Interface(cls_name, InterfaceGroup.DictLike, name, doc)
+            elif name.startswith('iter_'):
+                yield Interface(cls_name, InterfaceGroup.Iterator, name, doc)
+
+                for field in cls.ATTR_ITER_NODE:
+                    display = f'{name}(axis).{field}'
+                    doc = cls.scrub_doc(getattr(IterNodeDelegate, field).__doc__)
+                    yield Interface(cls_name, InterfaceGroup.Iterator, display, doc)
+
+            elif callable(obj):
+                if name_attr in _UFUNC_UNARY_OPERATORS:
+                    yield Interface(cls_name, InterfaceGroup.OperatorUnary, name, doc)
+                elif name_attr in _UFUNC_BINARY_OPERATORS or name_attr in _RIGHT_OPERATOR_MAP:
+                    yield Interface(cls_name, InterfaceGroup.OperatorBinary, name, doc)
+                else:
+                    yield Interface(cls_name, InterfaceGroup.Method, name, doc)
+            else:
+                yield Interface(cls_name, InterfaceGroup.Attribute, name, doc)
+
+    @classmethod
+    def to_frame(cls, target):
+        f = Frame.from_records(cls.interrogate(target))
+        f = f.sort_values(('cls', 'group', 'name'))
+        return f
 
 if __name__ == '__main__':
 
-
-    f = Frame.from_records(interrogate(Series))
-
-    f = f.sort_values(('cls', 'group', 'name'))
-    print(f.display(DisplayConfigs.UNBOUND))
+    print(InterfaceSummary.to_frame(Series).display(DisplayConfigs.UNBOUND))
---
+++ static_frame/test/unit/test_interface.py
@@ -0,0 +1,30 @@
+
+import unittest
+# import numpy as np  # type: ignore
+
+
+from static_frame.core.interface import InterfaceSummary
+from static_frame.core.series import Series
+from static_frame.core.frame import Frame
+from static_frame.core.index import Index
+from static_frame.core.index_hierarchy import IndexHierarchy
+
+from static_frame.test.test_case import TestCase
+
+
+
+class TestUnit(TestCase):
+
+    def test_interface_summary(self) -> None:
+
+        for target in (Series, Frame, Index, IndexHierarchy):
+            t = InterfaceSummary.to_frame(target)
+            self.assertTrue(len(t) > 30)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
