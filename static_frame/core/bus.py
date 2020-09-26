ee579d65337d51573d01c01200454b05e8b4ba30
flexatone <ariza@flexatone.com>
2020-09-26 07:32:48 -0700

    created StoreClientMixin to reuse Store constructors, exporters, refs #256

--- static_frame/core/bus.py
+++ static_frame/core/bus.py
@@ -7,8 +7,8 @@ import numpy as np
 from static_frame.core.container import ContainerBase
 from static_frame.core.display import Display
 from static_frame.core.display import DisplayActive
-from static_frame.core.display_config import DisplayConfig
 from static_frame.core.display import DisplayHeader
+from static_frame.core.display_config import DisplayConfig
 from static_frame.core.doc_str import doc_inject
 from static_frame.core.exception import ErrorInitBus
 from static_frame.core.frame import Frame
@@ -17,21 +17,13 @@ from static_frame.core.series import Series
 from static_frame.core.store import Store
 from static_frame.core.store import StoreConfigMap
 from static_frame.core.store import StoreConfigMapInitializer
-from static_frame.core.store_hdf5 import StoreHDF5
-from static_frame.core.store_sqlite import StoreSQLite
-from static_frame.core.store_xlsx import StoreXLSX
-from static_frame.core.store_zip import StoreZipCSV
-from static_frame.core.store_zip import StoreZipPickle
-from static_frame.core.store_zip import StoreZipTSV
-from static_frame.core.store_zip import StoreZipParquet
+from static_frame.core.store_client_base import StoreClientMixin
 from static_frame.core.util import DTYPE_BOOL
 from static_frame.core.util import DTYPE_FLOAT_DEFAULT
 from static_frame.core.util import DTYPE_OBJECT
 from static_frame.core.util import GetItemKeyType
-from static_frame.core.util import NULL_SLICE
-from static_frame.core.util import PathSpecifier
 from static_frame.core.util import NameType
-
+from static_frame.core.util import NULL_SLICE
 
 #-------------------------------------------------------------------------------
 class FrameDefferedMeta(type):
@@ -44,7 +36,7 @@ class FrameDeferred(metaclass=FrameDefferedMeta):
     '''
 
 #-------------------------------------------------------------------------------
-class Bus(ContainerBase): # not a ContainerOperand
+class Bus(ContainerBase, StoreClientMixin): # not a ContainerOperand
 
     __slots__ = (
         '_loaded',
@@ -84,130 +76,6 @@ class Bus(ContainerBase): # not a ContainerOperand
                     name=name,
                     )
         return cls(series, config=config)
-
-    #---------------------------------------------------------------------------
-    # constructors by data format
-
-    @classmethod
-    @doc_inject(selector='bus_constructor')
-    def from_zip_tsv(cls,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None,
-            ) -> 'Bus':
-        '''
-        Given a file path to zipped TSV :obj:`Bus` store, return a :obj:`Bus` instance.
-
-        {args}
-        '''
-        # take and store a StoreConfigMap
-        store = StoreZipTSV(fp)
-        return cls(cls._deferred_series(store.labels()),
-                store=store,
-                config=config
-                )
-
-    @classmethod
-    @doc_inject(selector='bus_constructor')
-    def from_zip_csv(cls,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> 'Bus':
-        '''
-        Given a file path to zipped CSV :obj:`Bus` store, return a :obj:`Bus` instance.
-
-        {args}
-        '''
-        store = StoreZipCSV(fp)
-        return cls(cls._deferred_series(store.labels()),
-                store=store,
-                config=config
-                )
-
-    @classmethod
-    @doc_inject(selector='bus_constructor')
-    def from_zip_pickle(cls,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> 'Bus':
-        '''
-        Given a file path to zipped pickle :obj:`Bus` store, return a :obj:`Bus` instance.
-
-        {args}
-        '''
-        store = StoreZipPickle(fp)
-        return cls(cls._deferred_series(store.labels()),
-                store=store,
-                config=config
-                )
-
-    @classmethod
-    @doc_inject(selector='bus_constructor')
-    def from_zip_parquet(cls,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> 'Bus':
-        '''
-        Given a file path to zipped parquet :obj:`Bus` store, return a :obj:`Bus` instance.
-
-        {args}
-        '''
-        store = StoreZipParquet(fp)
-        return cls(cls._deferred_series(store.labels()),
-                store=store,
-                config=config
-                )
-
-    @classmethod
-    @doc_inject(selector='bus_constructor')
-    def from_xlsx(cls,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> 'Bus':
-        '''
-        Given a file path to an XLSX :obj:`Bus` store, return a :obj:`Bus` instance.
-
-        {args}
-        '''
-        # how to pass configuration for multiple sheets?
-        store = StoreXLSX(fp)
-        return cls(cls._deferred_series(store.labels()),
-                store=store,
-                config=config
-                )
-
-    @classmethod
-    @doc_inject(selector='bus_constructor')
-    def from_sqlite(cls,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> 'Bus':
-        '''
-        Given a file path to an SQLite :obj:`Bus` store, return a :obj:`Bus` instance.
-
-        {args}
-        '''
-        store = StoreSQLite(fp)
-        return cls(cls._deferred_series(store.labels()),
-                store=store,
-                config=config
-                )
-
-    @classmethod
-    @doc_inject(selector='bus_constructor')
-    def from_hdf5(cls,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> 'Bus':
-        '''
-        Given a file path to a HDF5 :obj:`Bus` store, return a :obj:`Bus` instance.
-
-        {args}
-        '''
-        store = StoreHDF5(fp)
-        return cls(cls._deferred_series(store.labels()),
-                store=store,
-                config=config
-                )
 
     #---------------------------------------------------------------------------
     def __init__(self,
@@ -539,103 +407,3 @@ class Bus(ContainerBase): # not a ContainerOperand
                 return False
 
         return True
-
-    #---------------------------------------------------------------------------
-    # exporters
-
-    @doc_inject(selector='bus_exporter')
-    def to_zip_tsv(self,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> None:
-        '''
-        Write the complete :obj:`Bus` as a zipped archive of TSV files.
-
-        {args}
-        '''
-        store = StoreZipTSV(fp)
-        config = config if not None else self._config
-        store.write(self.items(), config=config)
-
-    @doc_inject(selector='bus_exporter')
-    def to_zip_csv(self,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> None:
-        '''
-        Write the complete :obj:`Bus` as a zipped archive of CSV files.
-
-        {args}
-        '''
-        store = StoreZipCSV(fp)
-        config = config if not None else self._config
-        store.write(self.items(), config=config)
-
-    @doc_inject(selector='bus_exporter')
-    def to_zip_pickle(self,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> None:
-        '''
-        Write the complete :obj:`Bus` as a zipped archive of pickles.
-
-        {args}
-        '''
-        store = StoreZipPickle(fp)
-        # config must be None for pickels, will raise otherwise
-        store.write(self.items(), config=config)
-
-    @doc_inject(selector='bus_exporter')
-    def to_zip_parquet(self,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> None:
-        '''
-        Write the complete :obj:`Bus` as a zipped archive of parquet files.
-
-        {args}
-        '''
-        store = StoreZipParquet(fp)
-        store.write(self.items(), config=config)
-
-    @doc_inject(selector='bus_exporter')
-    def to_xlsx(self,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> None:
-        '''
-        Write the complete :obj:`Bus` as a XLSX workbook.
-
-        {args}
-        '''
-        store = StoreXLSX(fp)
-        config = config if not None else self._config
-        store.write(self.items(), config=config)
-
-    @doc_inject(selector='bus_exporter')
-    def to_sqlite(self,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> None:
-        '''
-        Write the complete :obj:`Bus` as an SQLite database file.
-
-        {args}
-        '''
-        store = StoreSQLite(fp)
-        config = config if not None else self._config
-        store.write(self.items(), config=config)
-
-    @doc_inject(selector='bus_exporter')
-    def to_hdf5(self,
-            fp: PathSpecifier,
-            config: StoreConfigMapInitializer = None
-            ) -> None:
-        '''
-        Write the complete :obj:`Bus` as an HDF5 table.
-
-        {args}
-        '''
-        store = StoreHDF5(fp)
-        config = config if not None else self._config
-        store.write(self.items(), config=config)
