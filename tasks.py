a19b09e710a7d3e506b2a8759e09fa1a52b37672
flexatone <ariza@flexatone.com>
2020-12-02 15:05:48 -0800

    added coverage to GH Test WF; added --cov to tasks.py test interface

--- tasks.py
+++ tasks.py
@@ -10,6 +10,7 @@ import invoke
 def clean(context):
     '''Clean doc and build artifacts
     '''
+    context.run('rm -rf coverage.xml')
     context.run('rm -rf htmlcov')
     context.run('rm -rf doc/build')
     context.run('rm -rf build')
@@ -96,7 +97,10 @@ def example(context, container=None):
 #-------------------------------------------------------------------------------
 
 @invoke.task
-def test(context, unit=False, filename=None):
+def test(context,
+        unit=False,
+        cov=False,
+        ):
     '''Run tests.
     '''
     if unit:
@@ -104,10 +108,11 @@ def test(context, unit=False, filename=None):
     else:
         fp = 'static_frame/test'
 
-    if filename:
-        fp = os.path.join(fp, filename)
-
     cmd = f'pytest -s --color no --disable-pytest-warnings --tb=native {fp}'
+
+    if cov:
+        cmd += ' --cov=static_frame --cov-report=xml'
+
     print(cmd)
     context.run(cmd)
 
