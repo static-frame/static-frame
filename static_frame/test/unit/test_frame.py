7ef58a4f1fb36cff5b651906537a8904b6fdc973
tomrutherford <tmrutherford@gmail.com>
2021-01-07 22:07:27 -0800

    Temporarily disable clipboard test

--- static_frame/test/unit/test_frame.py
+++ static_frame/test/unit/test_frame.py
@@ -6593,19 +6593,19 @@ class TestUnit(TestCase):
             self.assertEqualFrames(f1, f2)
 
     #---------------------------------------------------------------------------
-    @skip_linux_no_display #type: ignore
-    def test_frame_to_clipboard_a(self) -> None:
-        records = (
-                (2, 'a', False),
-                (3, 'b', False),
-                )
-        f1 = Frame.from_records(records,
-                columns=('r', 's', 't'),
-                index=('w', 'x'))
+    # @skip_linux_no_display #type: ignore
+    # def test_frame_to_clipboard_a(self) -> None:
+        # records = (
+                # (2, 'a', False),
+                # (3, 'b', False),
+                # )
+        # f1 = Frame.from_records(records,
+                # columns=('r', 's', 't'),
+                # index=('w', 'x'))
 
-        f1.to_clipboard()
-        f2 = Frame.from_clipboard(index_depth=1)
-        self.assertTrue(f2.equals(f1, compare_dtype=True))
+        # f1.to_clipboard()
+        # f2 = Frame.from_clipboard(index_depth=1)
+        # self.assertTrue(f2.equals(f1, compare_dtype=True))
 
     #---------------------------------------------------------------------------
     def test_frame_to_html_a(self) -> None:
