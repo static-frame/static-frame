b2e450db3db42516a6508f198a3b4a535f504f9a
flexatone <ariza@flexatone.com>
2021-02-18 10:47:44 -0800

    fixed types

--- static_frame/test/unit/test_platform.py
+++ static_frame/test/unit/test_platform.py
@@ -8,7 +8,7 @@ from static_frame.test.test_case import TestCase
 
 class TestUnit(TestCase):
 
-    def test_platform_a(self):
+    def test_platform_a(self) -> None:
         post = sf.Platform.to_series()
         self.assertTrue(post.shape[0] > 10)
 
