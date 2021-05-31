089064134770390edfc6499ba05b4d7d00a6bea2
flexatone <ariza@flexatone.com>
2021-05-31 06:22:47 -0700

    removed arraykit refs in type_blocks.py

--- static_frame/core/type_blocks.py
+++ static_frame/core/type_blocks.py
@@ -7,15 +7,24 @@ from functools import partial
 from copy import deepcopy
 
 import numpy as np
-from arraykit import column_1d_filter
-from arraykit import column_2d_filter
-from arraykit import immutable_filter
-from arraykit import mloc
-from arraykit import resolve_dtype
-from arraykit import resolve_dtype_iter
-from arraykit import row_1d_filter
-from arraykit import shape_filter
 
+# from arraykit import column_1d_filter
+# from arraykit import column_2d_filter
+# from arraykit import immutable_filter
+# from arraykit import mloc
+# from arraykit import resolve_dtype
+# from arraykit import resolve_dtype_iter
+# from arraykit import row_1d_filter
+# from arraykit import shape_filter
+
+from static_frame.core.util import column_1d_filter
+from static_frame.core.util import column_2d_filter
+from static_frame.core.util import immutable_filter
+from static_frame.core.util import mloc
+from static_frame.core.util import resolve_dtype
+from static_frame.core.util import resolve_dtype_iter
+from static_frame.core.util import row_1d_filter
+from static_frame.core.util import shape_filter
 
 from static_frame.core.container import ContainerOperand
 from static_frame.core.container_util import apply_binary_operator_blocks
