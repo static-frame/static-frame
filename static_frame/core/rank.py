a25c8642a28096dcaf8c88209b30d1fc6ade94f4
Christopher Ariza <ariza@flexatone.com>
2021-07-08 11:56:52 -0700

    extended rank.py and tests

--- static_frame/core/rank.py
+++ static_frame/core/rank.py
@@ -1,11 +1,11 @@
 import typing as tp
 
-from enum import Enum
+from enum import Enum, unique
 import numpy as np
 
-from static_frame.core.util import DEFAULT_STABLE_SORT_KIND
+from static_frame.core.util import DEFAULT_STABLE_SORT_KIND, union1d
 from static_frame.core.util import DTYPE_INT_DEFAULT
-
+from static_frame.core.util import DTYPE_BOOL
 
 class RankMethod(str, Enum):
     AVERAGE = 'average'
@@ -14,37 +14,67 @@ class RankMethod(str, Enum):
     DENSE = 'dense'
     ORDINAL = 'ordinal'
 
-# after https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.rankdata.html
 def rank_1d(
         array: np.ndarray,
         method: RankMethod,
         ascending: bool = True,
+        start: int = 0,
         ):
+    '''
+    Rank 1D array. Basedon the the scipy implementation:
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.rankdata.html
+
+
+    average: The average of the ranks that would have been assigned to
+    all the tied values is assigned to each value.
+    min: The minimum of the ranks that would have been assigned to all
+    the tied values is assigned to each value.  (This is also
+    referred to as "competition" ranking.)
+    max: The maximum of the ranks that would have been assigned to all
+    the tied values is assigned to each value.
+    dense: Like 'min', but the rank of the next highest element is
+    assigned the rank immediately after those assigned to the tied
+    elements.
+    ordinal: All values are given a distinct rank, corresponding to
+    the order that the values occur in `a`.
+    '''
+    size = len(array)
     index_sorted = np.argsort(array, kind=DEFAULT_STABLE_SORT_KIND)
-    if not ascending:
-        index_sorted = (len(index_sorted) - 1) - index_sorted
     if method == RankMethod.ORDINAL:
-        return index_sorted
+        # NOTE: not sure if this works for other rank types
+        if not ascending:
+            index_sorted = (size - 1) - index_sorted
+        return index_sorted + start
 
-
-    array_sorted = array[index_sorted] # order
-    obs = np.r_[True, array_sorted[1:] != array_sorted[:-1]]
-    dense = obs.cumsum()[index_sorted]
+    array_sorted = array[index_sorted] # order array
+    # createa a Boolean array showing unique values, first value is always True
+    is_unique = np.full(size, True, dtype=DTYPE_BOOL)
+    is_unique[1:] = array_sorted[1:] != array_sorted[:-1]
+    # cumsum used on is_unique to only increment when unique; then re-order; this always has 1 as the lowest value
+    dense = is_unique.cumsum()[index_sorted]
 
     if method == RankMethod.DENSE:
-        return dense
+        return dense - 1 + start
 
-    # cumulative counts of each unique value
-    count = np.r_[np.nonzero(obs)[0], len(obs)]
+    # indices where unique is true
+    unique_pos = np.nonzero(is_unique)[0]
+    size_unique = len(unique_pos)
+    # cumulative counts of each unique value, adding length as last value
+    count = np.full(size_unique + 1, size)
+    count[:size_unique] = unique_pos
 
+    # import ipdb; ipdb.set_trace()
     if method == RankMethod.MAX:
-        return count[dense]
+        return count[dense] - 1 + start
 
     if method == RankMethod.MIN:
-        return count[dense - 1] + 1
+        return count[dense - 1] + start
 
     if method == RankMethod.AVERAGE:
-        return .5 * (count[dense] + count[dense - 1] + 1)
+        ranks0 = (.5 * (count[dense] + count[dense - 1] + 1)) - 1
+        if not ascending:
+            ranks0 = (size - 1) - ranks0
+        return ranks0 + start
 
     raise NotImplementedError(f'no handling for {method}')
 
--- static_frame/test/unit/test_rank.py
+++ static_frame/test/unit/test_rank.py
@@ -9,22 +9,58 @@ from static_frame.core.rank import RankMethod
 
 class TestUnit(TestCase):
 
-    def test_rank_a(self) -> None:
+    def test_rank_ordinal_a(self) -> None:
         self.assertEqual(
                 rank_1d(np.array([3, 2, 6, 20]), RankMethod.ORDINAL).tolist(),
                 [1, 0, 2, 3]
                 )
+
+        self.assertEqual(
+                rank_1d(np.array([3, 2, 6, 20]), RankMethod.ORDINAL, start=1).tolist(),
+                [2, 1, 3, 4]
+                )
+
 
         a2 = rank_1d(np.array([3, 2, 6, 20]), RankMethod.ORDINAL, ascending=False)
         self.assertEqual(a2.tolist(),
                 [2, 3, 1, 0]
                 )
 
+    def test_rank_average_a(self) -> None:
 
-        # a3 = rank_1d(np.array([0, 2, 3, 2]), 'average', ascending=False)
-        # self.assertEqual(a3.tolist(),
-        #         [4.0, 5.0, 3.0, 2.0, 1.0]
-        #         )
+        a1 = rank_1d(np.array([0, 2, 3, 2]), 'average', ascending=True)
+        self.assertEqual(a1.tolist(),
+                [0.0, 1.5, 3.0, 1.5]
+                )
+
+        a2 = rank_1d(np.array([0, 2, 3, 2]), 'average', start=1)
+        self.assertEqual(a2.tolist(),
+                [1.0, 2.5, 4.0, 2.5]
+                )
+
+        a3 = rank_1d(np.array([0, 2, 3, 2]), 'average', start=1, ascending=False)
+        self.assertEqual(a3.tolist(),
+                [4.0, 2.5, 1.0, 2.5]
+                )
+
+
+    def test_rank_average_b(self) -> None:
+
+        a1 = rank_1d(np.array([0, 2, 5, 2, 2, 2]), 'average', ascending=True)
+        self.assertEqual(a1.tolist(),
+                [0.0, 2.5, 2.5, 2.5, 5.0, 2.5]
+                )
+
+        # import ipdb; ipdb.set_trace()
+        a2 = rank_1d(np.array([0, 2, 3, 2]), 'average', start=1)
+        self.assertEqual(a2.tolist(),
+                [1.0, 2.5, 4.0, 2.5]
+                )
+
+        a3 = rank_1d(np.array([0, 2, 3, 2]), 'average', start=1, ascending=False)
+        self.assertEqual(a3.tolist(),
+                [4.0, 2.5, 1.0, 2.5]
+                )
 
 
 
