5a19b4976d78038a294e111084be8694df751c71
Christopher Ariza <ariza@flexatone.com>
2021-07-08 21:47:44 -0700

    extended rank.py and tests

--- static_frame/core/rank.py
+++ static_frame/core/rank.py
@@ -39,44 +39,45 @@ def rank_1d(
     the order that the values occur in `a`.
     '''
     size = len(array)
+    ranks0_max = size - 1
+
     index_sorted = np.argsort(array, kind=DEFAULT_STABLE_SORT_KIND)
     if method == RankMethod.ORDINAL:
-        # NOTE: not sure if this works for other rank types
-        if not ascending:
-            index_sorted = (size - 1) - index_sorted
-        return index_sorted + start
+        ranks0 = index_sorted
+    else:
+        array_sorted = array[index_sorted] # order array
+        # createa a Boolean array showing unique values, first value is always True
+        is_unique = np.full(size, True, dtype=DTYPE_BOOL)
+        is_unique[1:] = array_sorted[1:] != array_sorted[:-1]
+        # cumsum used on is_unique to only increment when unique; then re-order; this always has 1 as the lowest value
+        dense = is_unique.cumsum()[index_sorted]
 
-    array_sorted = array[index_sorted] # order array
-    # createa a Boolean array showing unique values, first value is always True
-    is_unique = np.full(size, True, dtype=DTYPE_BOOL)
-    is_unique[1:] = array_sorted[1:] != array_sorted[:-1]
-    # cumsum used on is_unique to only increment when unique; then re-order; this always has 1 as the lowest value
-    dense = is_unique.cumsum()[index_sorted]
+        if method == RankMethod.DENSE:
+            ranks0 = dense - 1
+            ranks0_max = ranks0.max()
+        else:
+            # indices where unique is true
+            unique_pos = np.nonzero(is_unique)[0]
+            size_unique = len(unique_pos)
+            # cumulative counts of each unique value, adding length as last value
+            count = np.full(size_unique + 1, size)
+            count[:size_unique] = unique_pos
 
-    if method == RankMethod.DENSE:
-        return dense - 1 + start
+            if ((method == RankMethod.MAX and ascending)
+                    or (method == RankMethod.MIN and not ascending)):
+                ranks0 = count[dense] - 1
+            elif ((method == RankMethod.MIN and ascending)
+                    or (method == RankMethod.MAX and not ascending)):
+                ranks0 = count[dense - 1]
+            elif method == RankMethod.AVERAGE:
+                ranks0 = (.5 * (count[dense] + count[dense - 1] + 1)) - 1
+            else:
+                raise NotImplementedError(f'no handling for {method}')
 
-    # indices where unique is true
-    unique_pos = np.nonzero(is_unique)[0]
-    size_unique = len(unique_pos)
-    # cumulative counts of each unique value, adding length as last value
-    count = np.full(size_unique + 1, size)
-    count[:size_unique] = unique_pos
+    if not ascending:
+        ranks0 = ranks0_max - ranks0
+    return ranks0 + start
 
-    # import ipdb; ipdb.set_trace()
-    if method == RankMethod.MAX:
-        return count[dense] - 1 + start
-
-    if method == RankMethod.MIN:
-        return count[dense - 1] + start
-
-    if method == RankMethod.AVERAGE:
-        ranks0 = (.5 * (count[dense] + count[dense - 1] + 1)) - 1
-        if not ascending:
-            ranks0 = (size - 1) - ranks0
-        return ranks0 + start
-
-    raise NotImplementedError(f'no handling for {method}')
 
     # range_array = np.arange(len(array), dtype=DTYPE_INT_DEFAULT)
 
