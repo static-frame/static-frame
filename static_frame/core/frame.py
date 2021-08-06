bb01fb450cbfb2560dc77bf5fa5f79d2ee773388
flexatone <ariza@flexatone.com>
2021-08-06 13:36:33 -0700

    progress on dt64.txt

--- static_frame/core/frame.py
+++ static_frame/core/frame.py
@@ -3548,7 +3548,11 @@ class Frame(ContainerOperand):
             axis: int = 0,
             condition: tp.Callable[[np.ndarray], bool] = np.all) -> 'Frame':
         '''
-        Return a new Frame after removing rows (axis 0) or columns (axis 1) where condition is True, where condition is an NumPy ufunc that process the Boolean array returned by isna().
+        Return a new Frame after removing rows (axis 0) or columns (axis 1) where any or all values are NA (NaN or None). The condition is determined by a NumPy ufunc that process the Boolean array returned by isna(); the default is `np.all`.
+
+        Args:
+            axis:
+            condition:
         '''
         # returns Boolean areas that define axis to keep
         row_key, column_key = self._blocks.drop_missing_to_keep_locations(
@@ -3557,9 +3561,8 @@ class Frame(ContainerOperand):
                 func=isna_array,
                 )
 
-        # NOTE: if not values to drop and this is a Frame (not a FrameGO) we can return self as it is immutable
+        # NOTE: if no values to drop and this is a Frame (not a FrameGO) we can return self as it is immutable. only one of row_key, colum_Key will be an array
         if self.__class__ is Frame:
-            # only one of row_key, colum_Key will be an array
             if ((column_key is None and row_key.all()) or
                     (row_key is None and column_key.all())):
                 return self
