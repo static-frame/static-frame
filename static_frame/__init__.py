123e4fa2d1831c728aabdcf0003eab4089ef3e01
flexatone <ariza@flexatone.com>
2021-08-08 13:17:25 -0700

    0.8.17 RC 1

--- static_frame/__init__.py
+++ static_frame/__init__.py
@@ -107,6 +107,6 @@ from static_frame.core.util import PathSpecifierOrFileLike as PathSpecifierOrFil
 from static_frame.core.util import SeriesInitializer as SeriesInitializer
 
 
-__version__ = '0.8.16' # use -dev for new version in development
+__version__ = '0.8.17' # use -dev for new version in development
 
 
