6c4798bb7c44f54372fcef1f2f6da8d971e0c368
flexatone <ariza@flexatone.com>
2021-08-21 12:14:23 -0700

    fixed types

--- static_frame/test/unit/test_rank.py
+++ static_frame/test/unit/test_rank.py
@@ -13,7 +13,7 @@ class TestUnit(TestCase):
 
     def test_rank_method_a(self) -> None:
         with self.assertRaises(NotImplementedError):
-            rank_1d(np.array([3, 2, 6, 20]), None)
+            rank_1d(np.array([3, 2, 6, 20]), None) #type: ignore
 
     def test_rank_ordinal_a(self) -> None:
         self.assertEqual(
