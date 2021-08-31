77b05b55b148c6037e3dbf760f0d8d5847f83c98
flexatone <ariza@flexatone.com>
2021-08-31 08:41:53 -0700

    additional coverage of IndexDefaultFactory

--- static_frame/test/unit/test_index_auto.py
+++ static_frame/test/unit/test_index_auto.py
@@ -1,11 +1,14 @@
 
 import unittest
+import datetime
 
 from static_frame.core.index import Index
 from static_frame.core.index import IndexGO
+from static_frame.core.index_datetime import IndexDate
 
 from static_frame.test.test_case import TestCase
 from static_frame.core.index_auto import IndexAutoFactory
+from static_frame.core.index_auto import IndexDefaultFactory
 
 class TestUnit(TestCase):
 
@@ -42,6 +45,19 @@ class TestUnit(TestCase):
         self.assertEqual(len(idx1), 5)
         self.assertEqual(idx1.STATIC, True)
 
+
+    def test_index_auto_factory_from_optional_constructor(self) -> None:
+        initializer = 3
+        explicit_constructor = IndexDefaultFactory(name='foo')
+        default_constructor = IndexDate
+        post = IndexAutoFactory.from_optional_constructor(
+                initializer=initializer,
+                explicit_constructor=explicit_constructor,
+                default_constructor=default_constructor,
+                )
+        self.assertEqual(post.name, 'foo')
+        self.assertEqual(post.values.tolist(),
+            [datetime.date(1970, 1, 1), datetime.date(1970, 1, 2), datetime.date(1970, 1, 3)])
 
 
 if __name__ == '__main__':
