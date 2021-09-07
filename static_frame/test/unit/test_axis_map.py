9abb032d0efa6e91fa22fd528db48bf4a5228b7d
Charles Burkland <burkland@rallc.com>
2021-09-07 00:13:35 -0700

    Fixes fast tests.

--- static_frame/test/unit/test_axis_map.py
+++ static_frame/test/unit/test_axis_map.py
@@ -8,6 +8,7 @@ from static_frame.test.test_case import TestCase
 from static_frame.core.bus import Bus
 from static_frame.core.frame import Frame
 from static_frame.core.index import Index
+from static_frame.core.index_level import TreeNodeT
 from static_frame.core.index_hierarchy import IndexHierarchy
 from static_frame.core.exception import ErrorInitBus
 from static_frame.core.exception import ErrorInitQuilt
@@ -47,18 +48,29 @@ class TestUnit(TestCase):
         self.assertEqual(post.values.tolist(),
                 [['a', 'f1'], ['a', 'f2'], ['a', 'f3'], ['b', 'f4'], ['b', 'f5']])
 
+    def compare_trees(self, tree1: TreeNodeT, tree2: TreeNodeT) -> None:
+        self.assertSequenceEqual(tree1.keys(), tree2.keys())
+
+        for k, v1 in tree1.items():
+            v2 = tree2[k]
+            if isinstance(v1, Index):
+                self.assertTrue(v1.equals(v2))
+            else:
+                self.compare_trees(v1, v2)
+
     def test_bus_to_hierarchy_a(self) -> None:
         f1 = ff.parse('s(4,4)|v(int,float)|c(I, str)').rename('f1')
         f2 = ff.parse('s(4,4)|v(str)|c(I, str)').rename('f2')
         f3 = ff.parse('s(4,4)|v(bool)|c(I, str)').rename('f3')
         b1 = Bus.from_frames((f1, f2, f3), name='a')
 
-        indices = (0, 1, 2, 3)
-        columns = ("zZbu", "ztsv", "zUvW", "zkuW")
+        indices = Index((0, 1, 2, 3))
+        columns = Index(("zZbu", "ztsv", "zUvW", "zkuW"))
 
         for _, frame in b1.items():
-            self.assertSequenceEqual(indices, frame.index.values.tolist())
-            self.assertSequenceEqual(columns, frame.columns.values.tolist())
+            self.assertTrue(indices.equals(frame.index))
+            self.assertTrue(columns.equals(frame.columns))
+
 
         def test_assertions(axis: int, flag: bool) -> None:
             hierarchy, opposite = bus_to_hierarchy(b1, axis=axis, deepcopy_from_bus=flag, init_exception_cls=ErrorInitBus)
@@ -67,12 +79,12 @@ class TestUnit(TestCase):
                 expected_tree: tp.Dict[str, tp.Sequence[tp.Any]] = {
                     'f1': indices, 'f2': indices, 'f3': indices
                 }
-                expected_index = Index(columns)
+                expected_index = columns
             else:
-                expected_index = Index(indices)
+                expected_index = indices
                 expected_tree = {'f1': columns, 'f2': columns, 'f3': columns}
 
-            self.assertDictEqual(hierarchy.to_tree(), expected_tree)
+            self.compare_trees(hierarchy.to_tree(), expected_tree)
             self.assertTrue(expected_index.equals(opposite))
 
         for axis in (0, 1):
@@ -84,9 +96,9 @@ class TestUnit(TestCase):
         class CustomError(Exception):
             pass
 
-        tree1 = dict(a_I=(1,2,3), a_II=(1,2,3))
-        tree2 = dict(b_I=(1,2,3), b_II=(1,2,3))
-        tree3 = dict(c_I=(1,2,3), c_II=(1,2,3))
+        tree1 = dict(a_I=Index((1,2,3)), a_II=Index((1,2,3)))
+        tree2 = dict(b_I=Index((1,2,3)), b_II=Index((1,2,3)))
+        tree3 = dict(c_I=Index((1,2,3)), c_II=Index((1,2,3)))
         index1 = IndexHierarchy.from_tree(tree1)
         index2 = IndexHierarchy.from_tree(tree2)
         index3 = IndexHierarchy.from_tree(tree3)
@@ -100,7 +112,7 @@ class TestUnit(TestCase):
 
         def test_assertions(hierarchy: IndexHierarchy, opposite: Index) -> None:
             expected_tree = dict(f1=tree1, f2=tree2, f3=tree3)
-            self.assertDictEqual(hierarchy.to_tree(), expected_tree)
+            self.compare_trees(hierarchy.to_tree(), expected_tree)
             self.assertTrue(index1.equals(opposite))
 
         test_assertions(*bus_to_hierarchy(b1, axis=0, deepcopy_from_bus=False, init_exception_cls=CustomError))
