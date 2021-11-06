5deebeeb084991b6a494d30b0afccfb46f480801
flexatone <ariza@flexatone.com>
2021-11-06 15:14:23 -0700

    additional property tests of to_npy, from_npy

--- static_frame/test/property/test_container_util.py
+++ static_frame/test/property/test_container_util.py
@@ -1,32 +1,17 @@
 
 # import typing as tp
 import unittest
-# import operator
-# import os
-# import sqlite3
-# import gc
-
 import numpy as np
 from hypothesis import given
-# from arraykit import isna_element
 
 from static_frame.core.frame import Frame
 from static_frame.core.index_datetime import IndexDate
 from static_frame.core.container_util import NPYConverter
 from static_frame.core.util import DTYPE_INEXACT_KINDS
-
-# from static_frame.core.frame import FrameGO
-# from static_frame.core.interface import UFUNC_AXIS_SKIPNA
-# from static_frame.core.interface import UFUNC_BINARY_OPERATORS
-# from static_frame.core.interface import UFUNC_UNARY_OPERATORS
-# from static_frame.core.series import Series
 from static_frame.test.property import strategies as sfst
 
-# from static_frame.test.test_case import skip_win
 from static_frame.test.test_case import temp_file
 from static_frame.test.test_case import TestCase
-
-
 
 class TestUnit(TestCase):
 
