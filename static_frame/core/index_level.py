3f628d4bca9c90ab223e2fcbac18449cece2db84
Charles Burkland <charles.aburkland@gmail.com>
2021-11-11 16:41:23 -0800

    Fixes invalid assert message syntax

--- static_frame/core/index_level.py
+++ static_frame/core/index_level.py
@@ -388,7 +388,7 @@ class IndexLevel:
                 depth_reference=self._depth
             )
 
-        assert self.targets is not None, f"Invalid {depth_level=}"
+        assert self.targets is not None, f"Invalid depth_level={depth_level}"
 
         new_targets = np.empty(len(self.targets), dtype=DTYPE_OBJECT)
         for i, target in enumerate(self.targets):
