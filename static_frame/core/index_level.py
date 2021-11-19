c7e2c52d363c62f33a6adb319f2706f63c2d0402
flexatone <ariza@flexatone.com>
2021-11-19 14:46:13 -0800

    additional coverage for index_level.py

--- static_frame/core/index_level.py
+++ static_frame/core/index_level.py
@@ -931,7 +931,7 @@ class IndexLevelGO(IndexLevel):
         for type_self, type_other in zip(self.index_types(), level.index_types()):
             # self type should be a GO; if other is non-GO, or GO, we can accept it
             if not issubclass(type_self, type_other):
-                raise RuntimeError(f'level for extension does not have corresponding types: {type_self}, {type_other}') # COV_MISSING
+                raise RuntimeError(f'level for extension does not have corresponding types: {type_self}, {type_other}')
 
         # this will raise for duplicates
         self.index.extend(level.index.values)
