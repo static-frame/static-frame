631195c76d234a1d7788fd91a5ed3b2ebb242dc1
flexatone <ariza@flexatone.com>
2021-12-16 14:57:14 -0800

    updated scale

--- doc/source/articles/serialize.py
+++ doc/source/articles/serialize.py
@@ -217,7 +217,7 @@ class SFReadNPYMM(FileIOTest):
 
 #-------------------------------------------------------------------------------
 def scale(v):
-    return int(v * .1)
+    return int(v * 1)
 
 FF_wide_uniform = f's({scale(100)},{scale(10_000)})|v(float)|i(I,int)|c(I,str)'
 FF_wide_mixed   = f's({scale(100)},{scale(10_000)})|v(int,int,bool,float,float)|i(I,int)|c(I,str)'
