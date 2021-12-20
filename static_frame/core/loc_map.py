a8f1b76907def59e5e1665a635b9f19eb314697b
flexatone <ariza@flexatone.com>
2021-12-20 08:20:24 -0800

    fixed types

--- static_frame/core/loc_map.py
+++ static_frame/core/loc_map.py
@@ -50,18 +50,18 @@ class LocMap:
                 if field == SLICE_STEP_ATTR:
                     raise RuntimeError(f'Step cannot be {attr}')
                 # if we match the same dt64 unit, simply use label_to_pos, increment stop
-                if attr.dtype == labels.dtype:
+                if attr.dtype == labels.dtype: # type: ignore
                     pos: TypePos = label_to_pos(attr)
                     if pos is None:
                         # if same type, and that atter is not in labels, we fail, just as we do in then non-datetime64 case. Only when datetimes are given in a different unit are we "loose" about matching.
                         raise LocInvalid('Invalid loc given in a slice', attr, field)
                     if field == SLICE_STOP_ATTR:
-                        pos += 1 #type: ignore  # stop is inclusive
+                        pos += 1 # stop is inclusive
                 elif field == SLICE_START_ATTR:
                     # NOTE: as an optimization only for the start attr, we can try to convert attr to labels unit and see if there is a match; this avoids astyping the entire labels array
                     pos: TypePos = label_to_pos(attr.astype(labels.dtype)) #type: ignore
                     if pos is None: # we did not find a start position
-                        labels_astype = labels.astype(attr.dtype)
+                        labels_astype = labels.astype(attr.dtype) #type: ignore
                         matches = np.flatnonzero(labels_astype == attr)
                         if len(matches):
                             pos = matches[0]
@@ -71,7 +71,7 @@ class LocMap:
                     # NOTE: we do not want to convert attr to labels dtype and take the match as we want to get the last of all possible matches of labels at the attr unit
                     # NOTE: try to re-use labels_astype if possible
                     if labels_astype is None or labels_astype.dtype != attr.dtype:
-                        labels_astype = labels.astype(attr.dtype)
+                        labels_astype = labels.astype(attr.dtype) #type: ignore
                     matches = np.flatnonzero(labels_astype == attr)
                     if len(matches):
                         pos = matches[-1] + 1
@@ -140,10 +140,10 @@ class LocMap:
         if key.__class__ is np.datetime64:
             # if we have a single dt64, convert this to the key's unit and do a Boolean selection if the key is a less-granular unit
             if (labels.dtype == DTYPE_OBJECT
-                    and np.datetime_data(key.dtype)[0] in DTYPE_OBJECTABLE_DT64_UNITS):
-                key = key.astype(DTYPE_OBJECT)
-            elif labels_is_dt64 and key.dtype < labels.dtype:
-                key = labels.astype(key.dtype) == key
+                    and np.datetime_data(key.dtype)[0] in DTYPE_OBJECTABLE_DT64_UNITS): #type: ignore
+                key = key.astype(DTYPE_OBJECT) #type: ignore
+            elif labels_is_dt64 and key.dtype < labels.dtype: #type: ignore
+                key = labels.astype(key.dtype) == key #type: ignore
             # if not different type, keep it the same so as to do a direct, single element selection
 
         is_array = key.__class__ is np.ndarray
@@ -151,22 +151,22 @@ class LocMap:
 
         # can be an iterable of labels (keys) or an iterable of Booleans
         if is_array or is_list:
-            if is_array and key.dtype.kind == DTYPE_DATETIME_KIND:
+            if is_array and key.dtype.kind == DTYPE_DATETIME_KIND: #type: ignore
                 if (labels.dtype == DTYPE_OBJECT
-                        and np.datetime_data(key.dtype)[0] in DTYPE_OBJECTABLE_DT64_UNITS):
+                        and np.datetime_data(key.dtype)[0] in DTYPE_OBJECTABLE_DT64_UNITS): #type: ignore
                     # if key is dt64 and labels are object, then for objectable units we can convert key to object to permit matching in the AutoMap
                     # NOTE: tolist() is expected to be faster than astype object for smaller collections
-                    key = key.tolist()
+                    key = key.tolist() #type: ignore
                     is_array = False
                     is_list = True
-                elif labels_is_dt64 and key.dtype < labels.dtype:
+                elif labels_is_dt64 and key.dtype < labels.dtype: #type: ignore
                     # change the labels to the dt64 dtype, i.e., if the key is years, recast the labels as years, and do a Boolean selection of everything that matches each key
-                    labels_ref = labels.astype(key.dtype)
+                    labels_ref = labels.astype(key.dtype) # type: ignore
                     # NOTE: this is only correct if both key and labels are dt64, and key is a less granular unit, as the order in the key and will not be used
                     # let Boolean key advance to next branch
-                    key = reduce(OPERATORS['__or__'], (labels_ref == k for k in key))
+                    key = reduce(OPERATORS['__or__'], (labels_ref == k for k in key)) # type: ignore
 
-            if is_array and key.dtype == DTYPE_BOOL:
+            if is_array and key.dtype == DTYPE_BOOL: #type: ignore
                 if offset_apply:
                     return positions[key] + offset
                 return positions[key]
@@ -177,13 +177,13 @@ class LocMap:
             if partial_selection:
                 if offset_apply:
                     return [label_to_pos[k] + offset for k in key if k in label_to_pos] #type: ignore
-                return [label_to_pos[k] for k in key if k in label_to_pos]
+                return [label_to_pos[k] for k in key if k in label_to_pos] # type: ignore
             if offset_apply:
                 return [label_to_pos[k] + offset for k in key] #type: ignore
-            return [label_to_pos[k] for k in key]
+            return [label_to_pos[k] for k in key] # type: ignore
 
         # if a single element (an integer, string, or date, we just get the integer out of the map
         if offset_apply:
             return label_to_pos[key] + offset #type: ignore
-        return label_to_pos[key]
+        return label_to_pos[key] #type: ignore
 
