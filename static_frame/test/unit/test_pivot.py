1e3fc57709537d542f06f0570e7e359e65b0ccc2
flexatone <ariza@flexatone.com>
2022-02-11 11:49:32 -0800

    updated tests

--- static_frame/test/unit/test_pivot.py
+++ static_frame/test/unit/test_pivot.py
@@ -3,55 +3,55 @@ import frame_fixtures as ff
 from static_frame.test.test_case import TestCase
 from static_frame.core.index import Index
 from static_frame.core.index_hierarchy import IndexHierarchy
-from static_frame.core.pivot import pivot_records_items
+# from static_frame.core.pivot import pivot_records_items
 
 
 class TestUnit(TestCase):
 
-    def test_pivot_records_items_a(self) -> None:
-        frame = ff.parse('s(3,6)|v(int,str,bool)|c(I,str)|i(I,int)')
-        group_fields = ['zUvW',] # needs to be valif loc selection
-        group_depth = 1
-        data_fields = ['zkuW', 'z2Oo']
-        func_single = sum
-        func_map = None
-        loc_to_iloc = frame.columns.loc_to_iloc
-        post = tuple(pivot_records_items(
-                blocks=frame._blocks,
-                group_fields_iloc=loc_to_iloc(group_fields),
-                group_depth=group_depth,
-                data_fields_iloc=loc_to_iloc(data_fields),
-                func_single=func_single,
-                func_map=func_map,
-                func_no=False,
-                kind='quicksort',
-                ))
-        self.assertEqual(post,
-                ((False, [201945, 1]), (True, [129017, False])))
+#     def test_pivot_records_items_a(self) -> None:
+#         frame = ff.parse('s(3,6)|v(int,str,bool)|c(I,str)|i(I,int)')
+#         group_fields = ['zUvW',] # needs to be valif loc selection
+#         group_depth = 1
+#         data_fields = ['zkuW', 'z2Oo']
+#         func_single = sum
+#         func_map = None
+#         loc_to_iloc = frame.columns.loc_to_iloc
+#         post = tuple(pivot_records_items(
+#                 blocks=frame._blocks,
+#                 group_fields_iloc=loc_to_iloc(group_fields),
+#                 group_depth=group_depth,
+#                 data_fields_iloc=loc_to_iloc(data_fields),
+#                 func_single=func_single,
+#                 func_map=func_map,
+#                 func_no=False,
+#                 kind='quicksort',
+#                 ))
+#         self.assertEqual(post,
+#                 ((False, [201945, 1]), (True, [129017, False])))
 
-    def test_pivot_records_items_b(self) -> None:
-        frame = ff.parse('s(3,6)|v(int,str,bool)|c(I,str)|i(I,int)')
-        group_fields = ['zUvW',] # needs to be valif loc selection
-        group_depth = 1
-        data_fields = ['zkuW', 'z2Oo']
-        func_single = None
-        func_map = (('zkuW', sum), ('z2Oo', min))
-        loc_to_iloc = frame.columns.loc_to_iloc
+#     def test_pivot_records_items_b(self) -> None:
+#         frame = ff.parse('s(3,6)|v(int,str,bool)|c(I,str)|i(I,int)')
+#         group_fields = ['zUvW',] # needs to be valif loc selection
+#         group_depth = 1
+#         data_fields = ['zkuW', 'z2Oo']
+#         func_single = None
+#         func_map = (('zkuW', sum), ('z2Oo', min))
+#         loc_to_iloc = frame.columns.loc_to_iloc
 
-        post = tuple(pivot_records_items(
-                blocks=frame._blocks,
-                group_fields_iloc=loc_to_iloc(group_fields),
-                group_depth=group_depth,
-                data_fields_iloc=loc_to_iloc(data_fields),
-                func_single=func_single,
-                func_map=func_map,
-                func_no=False,
-                kind='quicksort',
-                ))
-        self.assertEqual(post,
-                ((False, [201945, 35021, 1, False]),
-                (True, [129017, 129017, False, False]))
-                )
+#         post = tuple(pivot_records_items(
+#                 blocks=frame._blocks,
+#                 group_fields_iloc=loc_to_iloc(group_fields),
+#                 group_depth=group_depth,
+#                 data_fields_iloc=loc_to_iloc(data_fields),
+#                 func_single=func_single,
+#                 func_map=func_map,
+#                 func_no=False,
+#                 kind='quicksort',
+#                 ))
+#         self.assertEqual(post,
+#                 ((False, [201945, 35021, 1, False]),
+#                 (True, [129017, 129017, False, False]))
+#                 )
 
     def test_pivot_core_a(self) -> None:
 
