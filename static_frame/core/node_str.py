ecde4b0abb4d89994c12f3fd15c012a88b56978b
flexatone <ariza@flexatone.com>
2022-02-26 10:50:32 -0800

    fixed return type

--- static_frame/core/node_str.py
+++ static_frame/core/node_str.py
@@ -562,14 +562,14 @@ class InterfaceBatchString(InterfaceBatch):
         self._batch_apply = batch_apply
 
     #---------------------------------------------------------------------------
-    def __getitem__(self,  key: GetItemKeyType) -> TContainer:
+    def __getitem__(self,  key: GetItemKeyType) -> 'Batch':
         '''
         Return a container with the provided selection or slice of each element.
         '''
         return self._batch_apply(lambda c: c.via_str.__getitem__(key))
 
 
-    def capitalize(self) -> TContainer:
+    def capitalize(self) -> 'Batch':
         '''
         Return a container with only the first character of each element capitalized.
         '''
@@ -578,7 +578,7 @@ class InterfaceBatchString(InterfaceBatch):
     def center(self,
             width: int,
             fillchar: str = ' '
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Return a container with its elements centered in a string of length ``width``.
         '''
@@ -588,7 +588,7 @@ class InterfaceBatchString(InterfaceBatch):
             sub: str,
             start: tp.Optional[int] = None,
             end: tp.Optional[int] = None
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Returns a container with the number of non-overlapping occurrences of substring sub in the optional range ``start``, ``end``.
         '''
@@ -597,7 +597,7 @@ class InterfaceBatchString(InterfaceBatch):
     def decode(self,
             encoding: tp.Optional[str] = None,
             errors: tp.Optional[str] = None,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Apply str.decode() to each element. Elements must be bytes.
         '''
@@ -606,7 +606,7 @@ class InterfaceBatchString(InterfaceBatch):
     def encode(self,
             encoding: tp.Optional[str] = None,
             errors: tp.Optional[str] = None,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Apply str.encode() to each element. Elements must be strings.
         '''
@@ -616,7 +616,7 @@ class InterfaceBatchString(InterfaceBatch):
             suffix: tp.Union[str, tp.Iterable[str]],
             start: tp.Optional[int] = None,
             end: tp.Optional[int] = None
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Returns a container with the number of non-overlapping occurrences of substring ``suffix`` (or an interable of suffixes) in the optional range ``start``, ``end``.
         '''
@@ -626,7 +626,7 @@ class InterfaceBatchString(InterfaceBatch):
             sub: str,
             start: tp.Optional[int] = None,
             end: tp.Optional[int] = None
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         For each element, return the lowest index in the string where substring ``sub`` is found.
         '''
@@ -636,67 +636,67 @@ class InterfaceBatchString(InterfaceBatch):
             sub: str,
             start: tp.Optional[int] = None,
             end: tp.Optional[int] = None
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Like ``find``, but raises ``ValueError`` when the substring is not found.
         '''
         return self._batch_apply(lambda c: c.via_str.index(sub, start, end))
 
-    def isalnum(self) -> TContainer:
+    def isalnum(self) -> 'Batch':
         '''
         Returns true for each element if all characters in the string are alphanumeric and there is at least one character, false otherwise.
         '''
         return self._batch_apply(lambda c: c.via_str.isalnum())
 
-    def isalpha(self) -> TContainer:
+    def isalpha(self) -> 'Batch':
         '''
         Returns true for each element if all characters in the string are alphabetic and there is at least one character, false otherwise.
         '''
         return self._batch_apply(lambda c: c.via_str.isalpha())
 
-    def isdecimal(self) -> TContainer:
+    def isdecimal(self) -> 'Batch':
         '''
         For each element, return True if there are only decimal characters in the element.
         '''
         return self._batch_apply(lambda c: c.via_str.isdecimal())
 
-    def isdigit(self) -> TContainer:
+    def isdigit(self) -> 'Batch':
         '''
         Returns true for each element if all characters in the string are digits and there is at least one character, false otherwise.
         '''
         return self._batch_apply(lambda c: c.via_str.isdigit())
 
-    def islower(self) -> TContainer:
+    def islower(self) -> 'Batch':
         '''
         Returns true for each element if all cased characters in the string are lowercase and there is at least one cased character, false otherwise.
         '''
         return self._batch_apply(lambda c: c.via_str.islower())
 
-    def isnumeric(self) -> TContainer:
+    def isnumeric(self) -> 'Batch':
         '''
         For each element in self, return True if there are only numeric characters in the element.
         '''
         return self._batch_apply(lambda c: c.via_str.isnumeric())
 
-    def isspace(self) -> TContainer:
+    def isspace(self) -> 'Batch':
         '''
         Returns true for each element if there are only whitespace characters in the string and there is at least one character, false otherwise.
         '''
         return self._batch_apply(lambda c: c.via_str.isspace())
 
-    def istitle(self) -> TContainer:
+    def istitle(self) -> 'Batch':
         '''
         Returns true for each element if the element is a titlecased string and there is at least one character, false otherwise.
         '''
         return self._batch_apply(lambda c: c.via_str.istitle())
 
-    def isupper(self) -> TContainer:
+    def isupper(self) -> 'Batch':
         '''
         Returns true for each element if all cased characters in the string are uppercase and there is at least one character, false otherwise.
         '''
         return self._batch_apply(lambda c: c.via_str.isupper())
 
-    def len(self) -> TContainer:
+    def len(self) -> 'Batch':
         '''
         Return the length of the string.
         '''
@@ -705,13 +705,13 @@ class InterfaceBatchString(InterfaceBatch):
     def ljust(self,
             width: int,
             fillchar: str = ' '
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Return a container with its elements ljusted in a string of length ``width``.
         '''
         return self._batch_apply(lambda c: c.via_str.ljust(width, fillchar))
 
-    def lower(self) -> TContainer:
+    def lower(self) -> 'Batch':
         '''
         Return an array with the elements of self converted to lowercase.
         '''
@@ -719,7 +719,7 @@ class InterfaceBatchString(InterfaceBatch):
 
     def lstrip(self,
             chars: tp.Optional[str] = None,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         For each element, return a copy with the leading characters removed.
         '''
@@ -727,7 +727,7 @@ class InterfaceBatchString(InterfaceBatch):
 
     def partition(self,
             sep: str,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Partition each element around ``sep``.
         '''
@@ -737,7 +737,7 @@ class InterfaceBatchString(InterfaceBatch):
             old: str,
             new: str,
             count: tp.Optional[int] = None,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Return a container with its elements replaced in a string of length ``width``.
         '''
@@ -747,7 +747,7 @@ class InterfaceBatchString(InterfaceBatch):
             sub: str,
             start: tp.Optional[int] = None,
             end: tp.Optional[int] = None
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         For each element, return the highest index in the string where substring ``sub`` is found, such that sub is contained within ``start``, ``end``.
         '''
@@ -757,7 +757,7 @@ class InterfaceBatchString(InterfaceBatch):
             sub: str,
             start: tp.Optional[int] = None,
             end: tp.Optional[int] = None
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Like ``rfind``, but raises ``ValueError`` when the substring ``sub`` is not found.
         '''
@@ -766,7 +766,7 @@ class InterfaceBatchString(InterfaceBatch):
     def rjust(self,
             width: int,
             fillchar: str = ' '
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Return a container with its elements rjusted in a string of length ``width``.
         '''
@@ -774,7 +774,7 @@ class InterfaceBatchString(InterfaceBatch):
 
     def rpartition(self,
             sep: str,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Partition (split) each element around the right-most separator.
         '''
@@ -783,7 +783,7 @@ class InterfaceBatchString(InterfaceBatch):
     def rsplit(self,
             sep: str,
             maxsplit: int = -1,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         For each element, return a tuple of the words in the string, using sep as the delimiter string.
         '''
@@ -791,7 +791,7 @@ class InterfaceBatchString(InterfaceBatch):
 
     def rstrip(self,
             chars: tp.Optional[str] = None,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         For each element, return a copy with the trailing characters removed.
         '''
@@ -800,7 +800,7 @@ class InterfaceBatchString(InterfaceBatch):
     def split(self,
             sep: str,
             maxsplit: int = -1,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         For each element, return a tuple of the words in the string, using sep as the delimiter string.
         '''
@@ -812,7 +812,7 @@ class InterfaceBatchString(InterfaceBatch):
             prefix: tp.Union[str, tp.Iterable[str]],
             start: tp.Optional[int] = None,
             end: tp.Optional[int] = None
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Returns a container with the number of non-overlapping occurrences of substring `prefix` (or an interable of prefixes) in the optional range ``start``, ``end``.
         '''
@@ -820,19 +820,19 @@ class InterfaceBatchString(InterfaceBatch):
 
     def strip(self,
             chars: tp.Optional[str] = None,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         For each element, return a copy with the leading and trailing characters removed.
         '''
         return self._batch_apply(lambda c: c.via_str.strip(chars))
 
-    def swapcase(self) -> TContainer:
+    def swapcase(self) -> 'Batch':
         '''
         Return a container with uppercase characters converted to lowercase and vice versa.
         '''
         return self._batch_apply(lambda c: c.via_str.swapcase())
 
-    def title(self) -> TContainer:
+    def title(self) -> 'Batch':
         '''
         Return a container with uppercase characters converted to lowercase and vice versa.
         '''
@@ -840,7 +840,7 @@ class InterfaceBatchString(InterfaceBatch):
 
     # translate: akward input
 
-    def upper(self) -> TContainer:
+    def upper(self) -> 'Batch':
         '''
         Return a container with uppercase characters converted to lowercase and vice versa.
         '''
@@ -848,7 +848,7 @@ class InterfaceBatchString(InterfaceBatch):
 
     def zfill(self,
             width: int,
-            ) -> TContainer:
+            ) -> 'Batch':
         '''
         Return the string left-filled with zeros.
         '''
