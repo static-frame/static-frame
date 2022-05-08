9eca1b8d3bc9090b43d4ebd1df00d4b6fe971aff
Christopher Ariza <ariza@flexatone.com>
2022-05-08 08:38:38 -0700

    0.9.3 RC 1

--- static_frame/__init__.py
+++ static_frame/__init__.py
@@ -107,4 +107,4 @@ from static_frame.core.util import KeyOrKeys as KeyOrKeys
 from static_frame.core.util import PathSpecifierOrFileLike as PathSpecifierOrFileLike
 from static_frame.core.util import SeriesInitializer as SeriesInitializer
 
-__version__ = '0.9.2' # use -dev for new version in development
+__version__ = '0.9.3' # use -dev for new version in development
