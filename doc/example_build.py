cdc5676d14f0e66c6b88fcc2fffb7f52e84a057f
Christopher Ariza <ariza@flexatone.com>
2022-06-28 16:04:52 -0700

    architecture refactor

--- doc/example_build.py
+++ doc/example_build.py
@@ -59,74 +59,161 @@ def calls_to_msg(calls, row: sf.Series) -> tp.Iterator[str]:
     yield f'#end_{cls.__name__}-{row["signature_no_args"]}'
     yield ''
 
+#-------------------------------------------------------------------------------
+class ExGen:
 
-def series_constructors(row: sf.Series) -> tp.Iterator[str]:
+    @classmethod
+    def group_to_method(cls,
+            ig: InterfaceGroup
+            ) -> tp.Callable[[sf.Series], tp.Iterator[str]]:
+        attr = str(ig).lower().replace(' ', '_').replace('-', '_')
+        return getattr(cls, attr)
 
-    cls = ContainerMap.str_to_cls(row['cls_name'])
-    icls = f'sf.{cls.__name__}' # interface cls
-    attr = row['signature_no_args'][:-2] # drop paren
-    iattr = f'{icls}.{attr}'
+    @staticmethod
+    def constructor(row: sf.Series) -> tp.Iterator[str]:
+        return
 
-    if attr == '__init__':
+    @staticmethod
+    def exporter(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+    @staticmethod
+    def attribute(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+    @staticmethod
+    def method(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+    @staticmethod
+    def dictionary_like(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+    @staticmethod
+    def display(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+    @staticmethod
+    def assignment(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+    @staticmethod
+    def selector(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+    @staticmethod
+    def iterator(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+    @staticmethod
+    def operator_binary(row: sf.Series) -> tp.Iterator[str]:
+        return
+
+
+class ExGenSeries(ExGen):
+
+    @staticmethod
+    def constructor(row: sf.Series) -> tp.Iterator[str]:
+
+        cls = ContainerMap.str_to_cls(row['cls_name'])
+        icls = f'sf.{cls.__name__}' # interface cls
+        attr = row['signature_no_args'][:-2] # drop paren
+        iattr = f'{icls}.{attr}'
+
+        if attr == '__init__':
+            yield f's = {icls}({kwa(SERIES_INIT_A)})'
+        elif attr == 'from_concat':
+            yield f's1 = {icls}({kwa(SERIES_INIT_A)})'
+            yield f's2 = {icls}({kwa(SERIES_INIT_B)})'
+            yield f's = {iattr}((s1, s2))'
+        elif attr == 'from_concat_items':
+            yield f's1 = {icls}({kwa(SERIES_INIT_A)})'
+            yield f's2 = {icls}({kwa(SERIES_INIT_B)})'
+            yield f"s = {iattr}((('x', s1), ('y', s2)))"
+        elif attr == 'from_dict':
+            yield f's = {iattr}(dict({kwa(SERIES_INIT_DICT_A)}))'
+        elif attr == 'from_element':
+            yield f's = {iattr}({kwa(SERIES_INIT_FROM_ELEMENT_A)})'
+        elif attr == 'from_items':
+            yield f's = {iattr}({kwa(SERIES_INIT_FROM_ITEMS_A)})'
+        elif attr == 'from_overlay':
+            yield f's1 = {icls}({kwa(SERIES_INIT_C)})'
+            yield f's1'
+            yield f's2 = {icls}({kwa(SERIES_INIT_D)})'
+            yield f"s = {iattr}((s1, s2))"
+        elif attr == 'from_pandas':
+            yield f'df = pd.Series({kwa(SERIES_INIT_A, arg_first=True)})'
+            yield f's = {iattr}(df)'
+        else:
+            raise NotImplementedError(f'no handling for {attr}')
+        yield f's'
+
+    @staticmethod
+    def exporter(row: sf.Series) -> tp.Iterator[str]:
+
+        cls = ContainerMap.str_to_cls(row['cls_name'])
+        icls = f'sf.{cls.__name__}' # interface cls
+        attr = row['signature_no_args'] # drop paren
+
         yield f's = {icls}({kwa(SERIES_INIT_A)})'
-    elif attr == 'from_concat':
-        yield f's1 = {icls}({kwa(SERIES_INIT_A)})'
-        yield f's2 = {icls}({kwa(SERIES_INIT_B)})'
-        yield f's = {iattr}((s1, s2))'
-    elif attr == 'from_concat_items':
-        yield f's1 = {icls}({kwa(SERIES_INIT_A)})'
-        yield f's2 = {icls}({kwa(SERIES_INIT_B)})'
-        yield f"s = {iattr}((('x', s1), ('y', s2)))"
-    elif attr == 'from_dict':
-        yield f's = {iattr}(dict({kwa(SERIES_INIT_DICT_A)}))'
-    elif attr == 'from_element':
-        yield f's = {iattr}({kwa(SERIES_INIT_FROM_ELEMENT_A)})'
-    elif attr == 'from_items':
-        yield f's = {iattr}({kwa(SERIES_INIT_FROM_ITEMS_A)})'
-    elif attr == 'from_overlay':
-        yield f's1 = {icls}({kwa(SERIES_INIT_C)})'
-        yield f's1'
-        yield f's2 = {icls}({kwa(SERIES_INIT_D)})'
-        yield f"s = {iattr}((s1, s2))"
-    elif attr == 'from_pandas':
-        yield f'df = pd.Series({kwa(SERIES_INIT_A, arg_first=True)})'
-        yield f's = {iattr}(df)'
-    else:
-        raise NotImplementedError(f'no handling for {attr}')
-    yield f's'
+        yield f's.{attr}()'
 
-def series_attribute(row: sf.Series) -> tp.Iterator[str]:
+    @staticmethod
+    def attribute(row: sf.Series) -> tp.Iterator[str]:
 
-    cls = ContainerMap.str_to_cls(row['cls_name'])
-    icls = f'sf.{cls.__name__}' # interface cls
-    attr = row['signature_no_args'] # drop paren
+        cls = ContainerMap.str_to_cls(row['cls_name'])
+        icls = f'sf.{cls.__name__}' # interface cls
+        attr = row['signature_no_args'] # drop paren
 
-    yield f's = {icls}({kwa(SERIES_INIT_A)})'
-    yield f's.{attr}'
+        yield f's = {icls}({kwa(SERIES_INIT_A)})'
+        yield f's.{attr}'
 
-def series_dictlike(row: sf.Series) -> tp.Iterator[str]:
+    @staticmethod
+    def dictionary_like(row: sf.Series) -> tp.Iterator[str]:
 
-    cls = ContainerMap.str_to_cls(row['cls_name'])
-    icls = f'sf.{cls.__name__}' # interface cls
-    attr = row['signature_no_args']
-    attr_func = row['signature_no_args'][:-2]
+        cls = ContainerMap.str_to_cls(row['cls_name'])
+        icls = f'sf.{cls.__name__}' # interface cls
+        attr = row['signature_no_args']
+        attr_func = row['signature_no_args'][:-2]
 
-    yield f's = {icls}({kwa(SERIES_INIT_A)})'
+        yield f's = {icls}({kwa(SERIES_INIT_A)})'
 
-    if attr == '__contains__()':
-        yield f"s.{attr_func}('a')"
-    elif attr == 'get()':
-        yield f"s.{attr_func}('a')"
-        yield f"s.{attr_func}('z', -1)"
-    elif attr == 'values':
-        yield f"s.{attr}"
-    elif attr == 'items()':
-        yield f"tuple(s.{attr_func}())"
-    else:
-        yield f's.{attr_func}()'
+        if attr == '__contains__()':
+            yield f"s.{attr_func}('a')"
+        elif attr == 'get()':
+            yield f"s.{attr_func}('a')"
+            yield f"s.{attr_func}('z', -1)"
+        elif attr == 'values':
+            yield f"s.{attr}"
+        elif attr == 'items()':
+            yield f"tuple(s.{attr_func}())"
+        else:
+            yield f's.{attr_func}()'
 
+    @staticmethod
+    def dictionary_like(row: sf.Series) -> tp.Iterator[str]:
 
-def gen_examples(target):
+        cls = ContainerMap.str_to_cls(row['cls_name'])
+        icls = f'sf.{cls.__name__}' # interface cls
+        attr = row['signature_no_args']
+        attr_func = row['signature_no_args'][:-2]
+
+        yield f's = {icls}({kwa(SERIES_INIT_A)})'
+
+        if attr == '__contains__()':
+            yield f"s.{attr_func}('a')"
+        elif attr == 'get()':
+            yield f"s.{attr_func}('a')"
+            yield f"s.{attr_func}('z', -1)"
+        elif attr == 'values':
+            yield f"s.{attr}"
+        elif attr == 'items()':
+            yield f"tuple(s.{attr_func}())"
+        else:
+            yield f's.{attr_func}()'
+
+#-------------------------------------------------------------------------------
+def gen_examples(target, exg: ExGen):
 
     sf.DisplayActive.set(DC)
 
@@ -136,18 +223,14 @@ def gen_examples(target):
             max_args=99, # +inf, but keep as int
             )
 
-    for row in inter.loc[inter['group'] == InterfaceGroup.Constructor].iter_series(axis=1):
-        calls = series_constructors(row)
-        msg.extend(calls_to_msg(calls, row))
-
-    for row in inter.loc[inter['group'] == InterfaceGroup.Attribute].iter_series(axis=1):
-        calls = series_attribute(row)
-        msg.extend(calls_to_msg(calls, row))
-
-    for row in inter.loc[inter['group'] == InterfaceGroup.DictLike].iter_series(axis=1):
-        calls = series_dictlike(row)
-        msg.extend(calls_to_msg(calls, row))
-
+    for ig in (InterfaceGroup.Constructor,
+            InterfaceGroup.Attribute,
+            InterfaceGroup.DictLike,
+            ):
+        func = exg.group_to_method(ig)
+        for row in inter.loc[inter['group'] == ig].iter_series(axis=1):
+            calls = func(row)
+            msg.extend(calls_to_msg(calls, row))
 
     for line in msg:
         print(line)
@@ -156,4 +239,4 @@ def gen_examples(target):
 
 
 if __name__ == '__main__':
-    gen_examples(sf.Series)
+    gen_examples(sf.Series, ExGenSeries)
