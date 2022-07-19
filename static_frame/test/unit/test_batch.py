c3d0f4c96c77f8340f41c880d64684feb1aad17b
flexatone <ariza@flexatone.com>
2022-07-18 18:35:52 -0700

    test of Batch.via_values

--- static_frame/test/unit/test_batch.py
+++ static_frame/test/unit/test_batch.py
@@ -1606,11 +1606,11 @@ class TestUnit(TestCase):
     def test_batch_via_values_apply(self) -> None:
         f1 = ff.parse('s(2,3)|v(int)|c(I,str)').rename('a')
         f2 = ff.parse('s(2,3)|v(int)|c(I,str)').rename('b')
-        post = Batch.from_frames((f1, f2)).via_values(np.cos).to_frame()
-        import ipdb; ipdb.set_trace()
-        self.assertEqual(post.to_pairs(),
-                (('zZbu', ((('a', 34715), 'Q'), (('a', -3648), 'l'), (('b', 34715), 'Q'), (('b', -3648), 'l'))), ('ztsv', ((('a', 34715), 'i'), (('a', -3648), 'C'), (('b', 34715), 'i'), (('b', -3648), 'C'))), ('zUvW', ((('a', 34715), 'v'), (('a', -3648), 'W'), (('b', 34715), 'v'), (('b', -3648), 'W'))))
+        post = Batch.from_frames((f1, f2)).via_values.apply(np.cos).to_frame()
+        self.assertEqual(round(post, 2).to_pairs(),
+                (('zZbu', ((('a', 0), -0.54), (('a', 1), 0.05), (('b', 0), -0.54), (('b', 1), 0.05))), ('ztsv', ((('a', 0), -0.96), (('a', 1), -0.54), (('b', 0), -0.96), (('b', 1), -0.54))), ('zUvW', ((('a', 0), -0.82), (('a', 1), 1.0), (('b', 0), -0.82), (('b', 1), 1.0))))
                 )
+
 
 
     #---------------------------------------------------------------------------
