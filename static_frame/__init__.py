a51527002840222dc266e9e5547c70dbf46ba7a4
Christopher Ariza <ariza@flexatone.com>
2022-08-03 18:43:53 -0700

    0.9.9 RC 2

--- static_frame/__init__.py
+++ static_frame/__init__.py
@@ -113,4 +113,4 @@ from static_frame.core.util import PathSpecifierOrFileLike as PathSpecifierOrFil
 from static_frame.core.util import SeriesInitializer as SeriesInitializer
 from static_frame.core.yarn import Yarn as Yarn
 
-__version__ = '0.9.8'
+__version__ = '0.9.9'
