259c038bc12b51fd37b1beae41a054dd4b1e6d34
Michael Peters <peters@is-peters-oryx.ra.local>
2022-09-19 09:17:22 -0700

    add more detailed getsizeof_total tests for predefined seen

--- static_frame/test/unit/test_util_getsizeof_total.py
+++ static_frame/test/unit/test_util_getsizeof_total.py
@@ -9,11 +9,11 @@ from static_frame.test.test_case import TestCase
 
 
 class TestUnit(TestCase):
-    def test_int(self) -> None:
+    def test_getsizeof_total_int(self) -> None:
         obj = 2
         self.assertEqual(getsizeof_total(obj), getsizeof(2))
 
-    def test_set(self) -> None:
+    def test_getsizeof_total_set(self) -> None:
         obj = set(['a', 'b', 4])
         self.assertEqual(
             getsizeof_total(obj),
@@ -23,7 +23,7 @@ class TestUnit(TestCase):
             ))
         )
 
-    def test_frozenset(self) -> None:
+    def test_getsizeof_total_frozenset(self) -> None:
         obj = frozenset(['a', 'b', 4])
         self.assertEqual(
             getsizeof_total(obj),
@@ -33,21 +33,21 @@ class TestUnit(TestCase):
             ))
         )
 
-    def test_np_array(self) -> None:
+    def test_getsizeof_total_np_array(self) -> None:
         obj = np.arange(3)
         self.assertEqual(
             getsizeof_total(obj),
             getsizeof(np.arange(3))
         )
 
-    def test_frozenautomap(self) -> None:
+    def test_getsizeof_total_frozenautomap(self) -> None:
         obj = FrozenAutoMap(['a', 'b', 'c'])
         self.assertEqual(
             getsizeof_total(obj),
             getsizeof(FrozenAutoMap(['a', 'b', 'c']))
         )
 
-    def test_dict(self) -> None:
+    def test_getsizeof_total_dict(self) -> None:
         obj = { 'a': 2, 'b': 3, 'c': (4, 5) }
         self.assertEqual(
             getsizeof_total(obj),
@@ -59,14 +59,14 @@ class TestUnit(TestCase):
             ))
         )
 
-    def test_tuple(self) -> None:
+    def test_getsizeof_total_tuple(self) -> None:
         obj = (2, 3, 4)
         self.assertEqual(getsizeof_total(obj), sum(getsizeof(e) for e in (
             2, 3, 4,
             (2, 3, 4),
         )))
 
-    def test_nested_tuple(self) -> None:
+    def test_getsizeof_total_nested_tuple(self) -> None:
         obj = (2, 'b', (2, 3))
         self.assertEqual(getsizeof_total(obj), sum(getsizeof(e) for e in (
             2, 'b', 3,
@@ -74,9 +74,9 @@ class TestUnit(TestCase):
             (2, 'b', (2, 3)),
         )))
 
-    def test_predefined_seen(self) -> None:
+    def test_getsizeof_total_predefined_seen(self) -> None:
         obj = (4, 5, (2, 8))
-        seen = set([id(2), id(3)])
+        seen = set((id(2), id(3)))
         self.assertEqual(getsizeof_total(obj, seen=seen), sum(getsizeof(e) for e in (
             4, 5,
             8,
@@ -84,29 +84,45 @@ class TestUnit(TestCase):
             (4, 5, (2, 8))
         )))
 
-    # TODO: Add predefined_seen where it includes all elements already
+    def test_getsizeof_total_predefined_seen_all_elements(self) -> None:
+        tup_a = (2, 8)
+        obj = (4, 5, tup_a)
+        seen = set((id(e) for e in (4, 5, 2, 8, tup_a, obj)))
+        self.assertEqual(getsizeof_total(obj, seen=seen), 0)
 
-    def test_larger_values_is_larger(self) -> None:
+    def test_getsizeof_total_predefined_seen_sub_elements(self) -> None:
+        tup_a = (2, 8)
+        obj = (4, 5, tup_a)
+        seen = set((id(e) for e in (4, 5, 2, 8, tup_a)))
+        self.assertEqual(getsizeof_total(obj, seen=seen), getsizeof(obj))
+
+    def test_getsizeof_total_predefined_seen_base_element(self) -> None:
+        tup_a = (2, 8)
+        obj = (4, 5, tup_a)
+        seen = set((id(obj),))
+        self.assertEqual(getsizeof_total(obj, seen=seen), 0)
+
+    def test_getsizeof_total_larger_values_is_larger(self) -> None:
         a = ('a', 'b', 'c')
         b = ('abc', 'def', 'ghi')
         self.assertTrue(getsizeof_total(a) < getsizeof_total(b))
 
-    def test_more_values_is_larger_a(self) -> None:
+    def test_getsizeof_total_more_values_is_larger_a(self) -> None:
         a = ('a', 'b', 'c')
         b = ('a', 'b', 'c', 'd')
         self.assertTrue(getsizeof_total(a) < getsizeof_total(b))
 
-    def test_more_values_is_larger_b(self) -> None:
+    def test_getsizeof_total_more_values_is_larger_b(self) -> None:
         a = ('a', 'b', 'c')
         b = 'd'
         self.assertTrue(getsizeof_total([a]) < getsizeof_total([a, b]))
 
-    def test_more_values_is_larger_nested_a(self) -> None:
+    def test_getsizeof_total_more_values_is_larger_nested_a(self) -> None:
         a = ('a', (2, (8, 9), 4), 'c')
         b = ('a', (2, (8, 9, 10), 4), 'c')
         self.assertTrue(getsizeof_total(a) < getsizeof_total(b))
 
-    def test_more_values_is_larger_nested_b(self) -> None:
+    def test_getsizeof_total_more_values_is_larger_nested_b(self) -> None:
         a = np.array(['a', [2, (8, 9), 4], 'c'], dtype=object)
         b = np.array(['a', [2, (8, 9, 10), 4], 'c'], dtype=object)
         self.assertTrue(getsizeof_total(a) < getsizeof_total(b))
