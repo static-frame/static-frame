ecb56ed8684ac9fb4018a2aa870c460baab2c332
Michael Peters <peters@is-peters-oryx.ra.local>
2022-09-22 09:13:26 -0700

    Remove empty line

--- static_frame/test/unit/test_util.py
+++ static_frame/test/unit/test_util.py
@@ -2737,6 +2737,5 @@ class TestUnit(TestCase):
         self.assertTrue(is_objectable_dt64(np.array(('0001-01-01',), dtype=DT64_DAY)))
         self.assertTrue(is_objectable_dt64(np.array(('9999-12-31',), dtype=DT64_MS)))
 
-
 if __name__ == '__main__':
     unittest.main()
