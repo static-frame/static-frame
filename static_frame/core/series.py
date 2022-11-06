110ac249852e57ffd1bb5da2e6360a3ca3f5eaee
Christopher Ariza <ariza@flexatone.com>
2022-11-06 15:46:59 -0800

    extended parameers for Series

--- static_frame/core/series.py
+++ static_frame/core/series.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 from functools import partial
 from itertools import chain
 from itertools import product
+import csv
 
 import numpy as np
 from arraykit import delimited_to_arrays
@@ -217,7 +218,12 @@ class Series(ContainerOperand):
             dtype: DtypeSpecifier = None,
             name: NameType = None,
             index_constructor: tp.Optional[IndexConstructor] = None,
-            thousands_char: str = ',',
+            skip_initial_space: bool = False,
+            quoting: int = csv.QUOTE_MINIMAL,
+            quote_char: str = '"',
+            quote_double: bool = True,
+            escape_char: tp.Optional[str] = None,
+            thousands_char: str = '',
             decimal_char: str = '.',
             own_index: bool = False,
             ) -> 'Series':
@@ -231,8 +237,13 @@ class Series(ContainerOperand):
                 (delimited,), # make into iterable of one string
                 dtypes=get_col_dtype,
                 delimiter=delimiter,
+                quoting=quoting,
+                quotechar=quote_char,
+                doublequote=quote_double,
+                escapechar=escape_char,
                 thousandschar=thousands_char,
                 decimalchar=decimal_char,
+                skipinitialspace=skip_initial_space,
                 )
         if own_index:
             index_final = index
