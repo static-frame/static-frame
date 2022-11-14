24bfe627bdb05d67f6b63cc262a40a059216d11e
Christopher Ariza <ariza@flexatone.com>
2022-11-14 13:59:37 -0800

    fixed formatting

--- static_frame/test/property/test_frame.py
+++ static_frame/test/property/test_frame.py
@@ -8,6 +8,7 @@ import numpy as np
 from arraykit import isna_element
 from hypothesis import given
 
+from static_frame.core.exception import ErrorInitIndexNonUnique
 from static_frame.core.frame import Frame
 from static_frame.core.frame import FrameGO
 from static_frame.core.interface import UFUNC_AXIS_SKIPNA
@@ -19,7 +20,7 @@ from static_frame.test.property import strategies as sfst
 from static_frame.test.test_case import TestCase
 from static_frame.test.test_case import skip_win
 from static_frame.test.test_case import temp_file
-from static_frame.core.exception import ErrorInitIndexNonUnique
+
 
 class TestUnit(TestCase):
 
