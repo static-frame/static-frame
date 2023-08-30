528764a3684e71027574c53a531d150b8a2b27c9
Christopher Ariza <ariza@flexatone.com>
2023-08-29 19:20:51 -0700

    code cleanup

--- static_frame/core/util.py
+++ static_frame/core/util.py
@@ -43,10 +43,9 @@ if tp.TYPE_CHECKING:
     from static_frame.core.frame import Frame  # pylint: disable=W0611 #pragma: no cover
     from static_frame.core.index import Index  # pylint: disable=W0611 #pragma: no cover
     from static_frame.core.index_auto import IndexAutoConstructorFactory  # pylint: disable=W0611 #pragma: no cover
-    from static_frame.core.index_auto import IndexAutoFactory  # pylint: disable=W0611 #pragma: no cover
+    # from static_frame.core.index_auto import IndexAutoFactory  # pylint: disable=W0611 #pragma: no cover
     from static_frame.core.index_auto import IndexConstructorFactoryBase  # pylint: disable=W0611 #pragma: no cover
     from static_frame.core.index_base import IndexBase  # pylint: disable=W0611 #pragma: no cover
-    from static_frame.core.node_iter import IterNodeAxisElement
     from static_frame.core.series import Series  # pylint: disable=W0611 #pragma: no cover
     from static_frame.core.type_blocks import TypeBlocks  # pylint: disable=W0611 #pragma: no cover
     NDArrayAny = np.ndarray[tp.Any, tp.Any] # pylint: disable=W0611 #pragma: no cover
