561bf92ff174315f90c1b1a7082fb4905a72965b
Christopher Ariza <ariza@flexatone.com>
2023-09-06 11:08:06 -0700

    added typing test for index

---
+++ static_frame/test/typing/test_index.py
@@ -0,0 +1,17 @@
+import typing as tp
+
+import static_frame as sf
+
+
+
+def test_index_len_a() -> None:
+
+    idx = sf.Index(('a', 'b', 'c'))
+    l: int = len(idx)
+    assert l == 3
+
+def test_index_len_b() -> None:
+
+    idx = sf.Frame.from_records([('a', 'b'), ('c', 'd')])
+    l: int = len(idx.columns)
+    assert l == 2
