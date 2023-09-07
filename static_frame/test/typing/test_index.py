18f203b6e5192f0074a7959fbe4b32d83f282f4f
Christopher Ariza <ariza@flexatone.com>
2023-09-07 07:55:56 -0700

    added additional typing tests

--- static_frame/test/typing/test_index.py
+++ static_frame/test/typing/test_index.py
@@ -1,6 +1,7 @@
 import typing as tp
 
 import static_frame as sf
+import numpy as np
 
 
 def test_index_len_a() -> None:
@@ -14,3 +15,38 @@ def test_index_len_b() -> None:
     idx = sf.Frame.from_records([('a', 'b'), ('c', 'd')])
     l: int = len(idx.columns)
     assert l == 2
+
+
+def test_index_c() -> None:
+
+    idx = sf.Index(('a', 'b', 'c'))
+    assert len(idx) == 3
+
+    x1: str = idx[1]
+
+    x2: sf.Index = idx[1:]
+    x3: sf.Index = idx[[0, 1]]
+    x4: sf.Index = idx[idx.values == 'b']
+
+
+def test_index_d() -> None:
+
+    idx = sf.IndexDate(('2021-01-01', '2022-02-03', '2023-05-03'))
+    assert len(idx) == 3
+    x1: np.datetime64 = idx[1]
+
+    x2: sf.IndexDate = idx[1:]
+    x3: sf.IndexDate = idx[[0, 1]]
+    x4: sf.IndexDate = idx[idx.values == 'b']
+
+
+def test_index_e() -> None:
+
+    idx = sf.IndexHierarchy.from_product(('a', 'b'), (10, 20))
+    assert len(idx) == 4
+
+    x1: tp.Tuple[str, int] = idx[1]
+
+    x2: sf.IndexHierarchy = idx[1:]
+    x3: sf.IndexHierarchy = idx[[0, 1]]
+    x4: sf.IndexHierarchy = idx[idx.values_at_depth(1) == 20]
