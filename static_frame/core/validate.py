9f96a74d665d660c9c59b063a64b0a7a7c249653
Christopher Ariza <ariza@flexatone.com>
2023-09-19 21:02:56 -0700

    validation tests

--- static_frame/core/validate.py
+++ static_frame/core/validate.py
@@ -1,4 +1,8 @@
-import typing as tp
+import types
+import typing
+from collections import deque
+
+import typing_extensions as tp
 
 from static_frame.core.container import ContainerBase
 
@@ -7,21 +11,50 @@ class ValidationError(TypeError):
     def __init__(self, pairs: tp.Sequence[tp.Tuple[tp.Any, tp.Any]]) -> None:
         pass
 
+
+def get_series_pairs():
+    # yield pairs to compare
+    pass
+
+
 def validate_pair(value: tp.Any, hint: tp.Any) -> None:
-    if hint is tp.Any:
-        return
 
-    if isinstance(hint, ContainerBase):
-        if isinstance(value, hint):
-            return
+    q = deque(((value, hint),))
+    log = []
 
-    elif isinstance(hint, tp._GenericAlias):
-        # have a generic container
-        pass
+    while q:
+        v, h = q.popleft()
+        # import ipdb; ipdb.set_trace()
+        if h is tp.Any:
+            continue
 
-    elif isinstance(hint, (tp._UnionGenericAlias, tp.UnionType)):
-        # _UnionGenericAlias comes from tp.Union, tp.UnionType from | expressions
-        # tp.Optional returns a _UnionGenericAlias
-        pass
+        if isinstance(h, ContainerBase):
+            if isinstance(value, h):
+                continue
+            else:
+                log.append((v, h))
+                continue
+
+        elif isinstance(h, typing._GenericAlias):
+            # have a generic container
+            origin = tp.get_origin(h)
+            if isinstance(origin, ContainerBase):
+                args = tp.get_args(h)
+                # next: enque v, origin to get a type check
+                # enque the component checks
+
+        elif isinstance(h, (typing._UnionGenericAlias, types.UnionType)):
+            # _UnionGenericAlias comes from tp.Union, UnionType from | expressions
+            # tp.Optional returns a _UnionGenericAlias
+            pass
+        else:
+            pass
+
+    if log:
+        raise TypeError(log)
 
 
+TVFunc = tp.TypeVar('TVFunc', bound=tp.Callable[..., tp.Any])
+
+def validate(func: TVFunc) -> TVFunc:
+    pass
--- static_frame/test/unit/test_validate.py
+++ static_frame/test/unit/test_validate.py
@@ -1,5 +1,16 @@
+import typing_extensions as tp
+import numpy as np
 import static_frame as sf
-
 from static_frame.core.validate import validate_pair
 
 
+def test_validate_pair_a():
+
+    validate_pair(sf.IndexDate(('2022-01-01',)), sf.IndexDate)
+    validate_pair(sf.IndexDate(('2022-01-01',)), tp.Any)
+
+def test_validate_pair_b():
+    v = sf.Series(('a', 'b'), index=sf.IndexDate(('2021-04-05', '2022-05-03')))
+    h = sf.Series[sf.IndexDate, np.str_]
+
+    validate_pair(v, h)
