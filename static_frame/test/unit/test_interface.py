b901f8952567810cccfdc976af42bebfbfa01aa9
Christopher Ariza <ariza@flexatone.com>
2023-10-11 16:03:56 -0700

    fixed test

--- static_frame/test/unit/test_interface.py
+++ static_frame/test/unit/test_interface.py
@@ -28,7 +28,7 @@ class TestUnit(TestCase):
 
         self.assertEqual(
             counts.to_pairs(),
-            (('Accessor Datetime', 22), ('Accessor Fill Value', 26), ('Accessor Hashlib', 10), ('Accessor Regular Expression', 7), ('Accessor String', 39), ('Accessor Transpose', 24), ('Accessor Type Clinic', 4), ('Accessor Values', 3), ('Assignment', 16), ('Attribute', 12), ('Constructor', 38), ('Dictionary-Like', 7), ('Display', 6), ('Exporter', 31), ('Iterator', 156), ('Method', 102), ('Operator Binary', 24), ('Operator Unary', 4), ('Selector', 13))
+            (('Accessor Datetime', 22), ('Accessor Fill Value', 26), ('Accessor Hashlib', 10), ('Accessor Regular Expression', 7), ('Accessor String', 39), ('Accessor Transpose', 24), ('Accessor Type Clinic', 5), ('Accessor Values', 3), ('Assignment', 16), ('Attribute', 12), ('Constructor', 38), ('Dictionary-Like', 7), ('Display', 6), ('Exporter', 31), ('Iterator', 156), ('Method', 102), ('Operator Binary', 24), ('Operator Unary', 4), ('Selector', 13))
             )
 
     def test_interface_summary_c(self) -> None:
