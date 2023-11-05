c574f69963b5a31d86e975eec4a6d6093792f181
Christopher Ariza <ariza@flexatone.com>
2023-11-05 15:11:39 -0800

    added generic_aliases.py

--- static_frame/core/generic_aliases.py
+++ static_frame/core/generic_aliases.py
@@ -6,17 +6,15 @@ from static_frame.core.frame import FrameHE
 from static_frame.core.index import Index
 from static_frame.core.index_hierarchy import IndexHierarchy
 from static_frame.core.series import Series
+from static_frame.core.series import SeriesHE
 
 TIndexAny = Index[tp.Any]
 TIndexHierarchyAny = IndexHierarchy[tp.Unpack[tp.Tuple[tp.Any, ...]]] # type: ignore[type-arg]
 
-TNDIndexAny = tp.Union[TIndexAny, TIndexHierarchyAny]
+TSeriesAny = Series[tp.Any, tp.Any]
+TSeriesHEAny = SeriesHE[tp.Any, tp.Any]
 
 
-TSeriesAny = Series[TNDIndexAny, tp.Any]
-
-
-
-TFrameAny = Frame[TNDIndexAny, TNDIndexAny, tp.Unpack[tp.Tuple[tp.Any, ...]]] # type: ignore[type-arg] # pylint: disable=W0611 #pragma: no cover
-TFrameGOAny = FrameGO[TNDIndexAny, TNDIndexAny, tp.Unpack[tp.Tuple[tp.Any, ...]]] # type: ignore[type-arg] # pylint: disable=W0611 #pragma: no cover
-TFrameHEAny = FrameHE[TNDIndexAny, TNDIndexAny, tp.Unpack[tp.Tuple[tp.Any, ...]]] # type: ignore[type-arg] # pylint: disable=W0611 #pragma: no cover
+TFrameAny = Frame[tp.Any, tp.Any, tp.Unpack[tp.Tuple[tp.Any, ...]]] # type: ignore[type-arg] # pylint: disable=W0611 #pragma: no cover
+TFrameGOAny = FrameGO[tp.Any, tp.Any, tp.Unpack[tp.Tuple[tp.Any, ...]]] # type: ignore[type-arg] # pylint: disable=W0611 #pragma: no cover
+TFrameHEAny = FrameHE[tp.Any, tp.Any, tp.Unpack[tp.Tuple[tp.Any, ...]]] # type: ignore[type-arg] # pylint: disable=W0611 #pragma: no cover
