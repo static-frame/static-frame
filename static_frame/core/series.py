2108660bfc8407e6a3990bd02a48284f5b5a002c
Christopher Ariza <ariza@flexatone.com>
2023-11-07 07:42:37 -0800

    silence pylint

--- static_frame/core/series.py
+++ static_frame/core/series.py
@@ -131,10 +131,10 @@ from static_frame.core.util import write_optional_file
 if tp.TYPE_CHECKING:
     import pandas  # pylint: disable=W0611 #pragma: no cover
 
-    from static_frame.core.generic_aliases import TBusAny  # pylint: disable=W0611 #pragma: no cover
-    from static_frame.core.generic_aliases import TFrameAny  # pylint: disable=W0611 #pragma: no cover
-    from static_frame.core.generic_aliases import TFrameGOAny  # pylint: disable=W0611 #pragma: no cover
-    from static_frame.core.generic_aliases import TFrameHEAny  # pylint: disable=W0611 #pragma: no cover
+    from static_frame.core.generic_aliases import TBusAny  # pylint: disable=C0412 #pragma: no cover
+    from static_frame.core.generic_aliases import TFrameAny  # pylint: disable=C0412 #pragma: no cover
+    from static_frame.core.generic_aliases import TFrameGOAny  # pylint: disable=C0412 #pragma: no cover
+    from static_frame.core.generic_aliases import TFrameHEAny  # pylint: disable=C0412 #pragma: no cover
 
     TNDArrayAny = np.ndarray[tp.Any, tp.Any] # pylint: disable=W0611 #pragma: no cover
     TDtypeAny = np.dtype[tp.Any] # pylint: disable=W0611 #pragma: no cover
