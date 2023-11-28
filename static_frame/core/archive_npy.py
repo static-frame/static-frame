1de456c0de4bcf4d29d005d6f722399e8487aca9
Christopher Ariza <ariza@flexatone.com>
2023-11-27 18:53:45 -0800

    coverage

--- static_frame/core/archive_npy.py
+++ static_frame/core/archive_npy.py
@@ -39,7 +39,7 @@ if tp.TYPE_CHECKING:
     import pandas as pd  # pylint: disable=W0611 #pragma: no cover
 
     from static_frame.core.frame import Frame  # pylint: disable=W0611,C0412 #pragma: no cover
-    from static_frame.core.generic_aliases import TFrameAny
+    from static_frame.core.generic_aliases import TFrameAny # pylint: disable=W0611,C0412 #pragma: no cover
 
     TNDArrayAny = np.ndarray[tp.Any, tp.Any] # pylint: disable=W0611 #pragma: no cover
     TDtypeAny = np.dtype[tp.Any] # pylint: disable=W0611 #pragma: no cover
