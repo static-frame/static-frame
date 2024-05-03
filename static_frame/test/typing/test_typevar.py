fa6645fc169e0a0d66e556fb20c1a3cf5d348030
flexatone <ariza@flexatone.com>
2024-05-03 13:46:18 -0700

    added more typevar tests

--- static_frame/test/typing/test_typevar.py
+++ static_frame/test/typing/test_typevar.py
@@ -1,23 +1,13 @@
+# pylint: disable=C0321
+
 import typing_extensions as tp
 
-
-class A:
-    pass
-
-class B1(A):
-    pass
-
-class B2(A):
-    pass
-
-class C:
-    pass
-
-class D1(C):
-    pass
-
-class D2(C):
-    pass
+class A: ...
+class B1(A): ...
+class B2(A): ...
+class C: ...
+class D1(C): ...
+class D2(C): ...
 
 def test_typevar_a1() -> None:
 
@@ -27,9 +17,6 @@ def test_typevar_a1() -> None:
         return x
 
     post: B1 = process(B1(), B1())
-    # post: B = process(B(), C()) # fails: "C" is incompatible with "B"
-    # post: C = process(C(), C()) # fails: "C" is incompatible with "A"
-
     # NOTE: this shows that the first-encountered type sets the type; it seems that Pyright uses the last-encountered value...
     # post: B1 = process(B1(), B2()) # fails: "B2" is incompatible with "B1"
     # post: B2 = process(B2(), B1()) # fails: "B1" is incompatible with "B2"
@@ -63,7 +50,7 @@ def test_typevar_b2() -> None:
 
     post: D1 = process(D1(), D1())
     # post: D1 = process(D1(), B1()) # fails: "B1" is incompatible with "D1"
-
+    # this shows the first observed value sets the type
 
 def test_typevar_c1() -> None:
     # https://stackoverflow.com/questions/59933946/difference-between-typevart-a-b-and-typevart-bound-uniona-b
@@ -126,3 +113,103 @@ def test_typevar_c3() -> None:
     b = concat1(mix1, mix3) # passes, which is surprising as we used D1 in mix2 above
     c = concat1(mix4, mix4)
     d = concat1(mix4, mix5)
+
+def test_type_clinic_typevar_d1() -> None:
+
+    class A: ...
+    class A1(A): ...
+    class A2(A): ...
+    class B: ...
+    class B1(B): ...
+    class B2(B): ...
+    class C: ...
+    class C1(C): ...
+    class C2(C): ...
+
+    T = tp.TypeVar('T', bound=tp.Union[A, C])
+    def process(a: tp.Tuple[T, T, T, T, T, T]) -> T:
+        return a[0]
+
+    x = process((A2(), C2(), C2(), A1(), C1(), C1())) # passes
+    # this shows that the union is not specialized, nor not set on first encuontered
+
+def test_type_clinic_typevar_d2() -> None:
+
+    class A: ...
+    class A1(A): ...
+    class A2(A): ...
+    class B: ...
+    class B1(B): ...
+    class B2(B): ...
+    class C: ...
+    class C1(C): ...
+    class C2(C): ...
+
+    T = tp.TypeVar('T', bound=tp.Union[A, B, C])
+    def process(a: T, b: T, c: T, d: T) -> T:
+        return a
+
+    x = process(A2(), C2(), B2(), C2()) # this passes
+    x = process(A1(), C2(), A2(), C1()) # this passes
+    # this shows that a union tyupe as bound is not specialized by subclass; the bound remains the requirements
+
+def test_type_clinic_typevar_d3() -> None:
+
+    class A: ...
+    class A1(A): ...
+    class A2(A): ...
+    class B: ...
+    class B1(B): ...
+    class B2(B): ...
+    class C: ...
+    class C1(C): ...
+    class C2(C): ...
+
+    T = tp.TypeVar('T', bound=tp.Union[A, B, C])
+    def process(a: T, b: T, c: T, d: T) -> T:
+        return a
+
+    x = process(A2(), C2(), B2(), C2()) # this passes
+    x = process(A1(), C2(), A2(), C1()) # this passes
+    # this shows that a union type as bound is not specialized by subclass; the bound remains the requirements
+
+def test_type_clinic_typevar_d4() -> None:
+
+    class A: ...
+    class A1(A): ...
+    class A2(A): ...
+
+    T = tp.TypeVar('T', bound=A)
+    def process(a: T, b: T) -> T:
+        return a
+
+    x = process(A1(), A1()) # this passes
+    y = process(A2(), A2()) # this passes
+    z = process(A2(), A1()) # this passes
+
+    # this shows that a union type as bound is not specialized by subclass; the bound remains the requirement
+
+def test_type_clinic_typevar_d5() -> None:
+
+    class A: ...
+    class A1(A): ...
+    class A2(A): ...
+
+    T = tp.TypeVar('T')
+    def process(a: T, b: T) -> T:
+        return a
+
+    x = process(A1(), A1()) # this passes
+    y = process(A2(), A2()) # this passes
+    z = process(A2(), A1()) # this passes
+
+def test_type_clinic_typevar_d6() -> None:
+
+    T = tp.TypeVar('T')
+    def process(a: T, b: T) -> T:
+        return b
+
+    x = process('a', 'a') # this passes
+    y = process(0, 0) # this passes
+    z = process('a', 0) # this passes, which does not make sense!
+    a = process(0, 'q') # this passes, which does not make sense!
