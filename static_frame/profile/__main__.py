9d81748085f00f73d3b01146d9cce799b9fdb62b
flexatone <ariza@flexatone.com>
2024-06-09 16:51:04 -0700

    additional join performance tests

--- static_frame/profile/__main__.py
+++ static_frame/profile/__main__.py
@@ -1046,31 +1046,36 @@ class JoinLeftMany(Perf):
     def __init__(self) -> None:
         super().__init__()
 
-        self.sff_left = ff.parse('s(1000,4)|v(int)|i(I,str)|c(I,str)').assign[sf.ILoc[0]].apply(lambda s: s % 4)
+        self.sff_left = ff.parse('s(10_000,4)|v(int)|i(I,str)|c(I,str)').assign[sf.ILoc[0]].apply(lambda s: s % 4)
         self.pdf_left = self.sff_left.to_pandas()
 
-        self.sff_right = ff.parse('s(20,3)|v(int,bool,bool)|i(I,str)').assign[sf.ILoc[0]].apply(lambda s: s % 4)
+        self.sff_right = ff.parse('s(200,3)|v(int,bool,bool)|i(I,str)').assign[sf.ILoc[0]].apply(lambda s: s % 4)
         self.pdf_right = self.sff_right.to_pandas()
 
         from static_frame.core.join import _join_trimap_target_one
         self.meta = {
-            'basic': FunctionMetaData(
+            'left_larger': FunctionMetaData(
                 line_target=_join_trimap_target_one,
                 perf_status=PerfStatus.UNEXPLAINED_WIN,
                 ),
             }
 
 class JoinLeftMany_N(JoinLeftMany, Native):
-
-    def basic(self) -> None:
+    def left_larger(self) -> None:
         post = self.sff_left.join_left(self.sff_right, left_columns='zZbu', right_columns=0)
-        assert post.shape == (5046, 7)
+        # assert post.shape == (5046, 7)
+
+    def right_larger(self) -> None:
+        post = self.sff_right.join_left(self.sff_left, right_columns='zZbu', left_columns=0)
+        # assert post.shape == (5046, 7)
 
 class JoinLeftMany_R(JoinLeftMany, Reference):
-
-    def basic(self) -> None:
+    def left_larger(self) -> None:
         post = self.pdf_left.merge(self.pdf_right, how='left', left_on='zZbu', right_on=0)
-        assert post.shape == (5046, 7)
+        # assert post.shape == (5046, 7)
+
+    def right_larger(self) -> None:
+        post = self.pdf_right.merge(self.pdf_left, how='left', right_on='zZbu', left_on=0)
 
 
 class JoinLeftUnique(Perf):
@@ -1079,7 +1084,7 @@ class JoinLeftUnique(Perf):
     def __init__(self) -> None:
         super().__init__()
 
-        self.sff_left = ff.parse('s(1000,4)|v(int)|i(I,str)|c(I,str)')
+        self.sff_left = ff.parse('s(1_000,4)|v(int)|i(I,str)|c(I,str)')
         self.pdf_left = self.sff_left.to_pandas()
 
         self.sff_right = ff.parse('s(500,3)|v(str,bool,int)')
@@ -1097,14 +1102,20 @@ class JoinLeftUnique_N(JoinLeftUnique, Native):
 
     def left_larger(self) -> None:
         post = self.sff_left.join_left(self.sff_right, left_depth_level=0, right_columns=0)
-        assert post.shape == (1000, 7)
+        # assert post.shape == (1000, 7)
+
+    def right_larger(self) -> None:
+        post = self.sff_right.join_left(self.sff_left, right_depth_level=0, left_columns=0)
 
 class JoinLeftUnique_R(JoinLeftUnique, Reference):
 
     def left_larger(self) -> None:
-        # NOTE: pandas creates the right_on column as a union of the left index and the original right values; not sure if this is correct
         post = self.pdf_left.merge(self.pdf_right, how='left', left_index=True, right_on=0)
-        assert post.shape == (1000, 7)
+        # assert post.shape == (1000, 7)
+
+    def right_larger(self) -> None:
+        post = self.pdf_right.merge(self.pdf_left, how='left', right_index=True, left_on=0)
+        # assert post.shape == (1000, 7)
 
 #-------------------------------------------------------------------------------
 
