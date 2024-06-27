608e579d73cd97538c2b0f66995254794a338e40
Christopher Ariza <ariza@flexatone.com>
2024-06-27 08:55:00 -0700

    completed test

--- static_frame/test/unit/test_store_duckdb.py
+++ static_frame/test/unit/test_store_duckdb.py
@@ -2,7 +2,7 @@ import numpy as np
 import frame_fixtures as ff
 
 from static_frame.core.store_duckdb import frame_to_connection
-
+from static_frame.core.frame import Frame
 
 
 def test_store_duckd_a():
@@ -12,6 +12,7 @@ def test_store_duckd_a():
     f1 = ff.parse('s(6,3)|v(int64,str,bool)|c(I,str)')
     conn = duckdb.connect()
     post = frame_to_connection(frame=f1, connection=conn)
-    # f2 = f.Frame.from_pandas(post.df())
-
-    # import ipdb; ipdb.set_trace()
+    f2 = Frame.from_pandas(post.df())
+    assert (f2.to_pairs() ==
+            (('zZbu', ((0, -88017), (1, 92867), (2, 84967), (3, 13448), (4, 175579), (5, 58768))), ('ztsv', ((0, 'zaji'), (1, 'zJnC'), (2, 'zDdR'), (3, 'zuVU'), (4, 'zKka'), (5, 'zJXD'))), ('zUvW', ((0, True), (1, False), (2, False), (3, True), (4, False), (5, False))))
+            )
