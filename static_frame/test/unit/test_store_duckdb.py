6507042bff6ea0b21e188f5e76ca6953cdd79ea6
Christopher Ariza <ariza@flexatone.com>
2024-06-27 09:32:45 -0700

    removed debug

--- static_frame/test/unit/test_store_duckdb.py
+++ static_frame/test/unit/test_store_duckdb.py
@@ -12,7 +12,6 @@ def test_store_duckd_a():
     f1 = ff.parse('s(6,3)|v(int64,str,bool)|c(I,str)')
     conn = duckdb.connect()
     post = frame_to_connection(frame=f1, connection=conn)
-    import ipdb; ipdb.set_trace()
     f2 = Frame.from_pandas(post.df())
     assert (f2.to_pairs() ==
             (('zZbu', ((0, -88017), (1, 92867), (2, 84967), (3, 13448), (4, 175579), (5, 58768))), ('ztsv', ((0, 'zaji'), (1, 'zJnC'), (2, 'zDdR'), (3, 'zuVU'), (4, 'zKka'), (5, 'zJXD'))), ('zUvW', ((0, True), (1, False), (2, False), (3, True), (4, False), (5, False))))
