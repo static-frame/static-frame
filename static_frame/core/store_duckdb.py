670ea5e78c57e514dc5b4a340b5e838e1b4eeb01
Christopher Ariza <ariza@flexatone.com>
2024-06-27 17:09:45 -0700

    extended conversion routines

--- static_frame/core/store_duckdb.py
+++ static_frame/core/store_duckdb.py
@@ -1,7 +1,6 @@
 import typing_extensions as tp
 
 from static_frame.core.generic_aliases import TFrameAny
-
 if tp.TYPE_CHECKING:
     from duckdb import DuckDBPyConnection
 
@@ -17,34 +16,60 @@ if tp.TYPE_CHECKING:
 # JOIN t3 ON t1.rownum = t3.rownum
 # '''
 
-def frame_to_connection(
-        *,
-        frame: TFrameAny,
-        # label: str, # can be None
-        connection: 'DuckDBPyConnection',
-        # include_index: bool,
-        ) -> 'DuckDBPyConnection':
-    '''
-    Args:
-        label: string to be used as the table name.
-    '''
+from static_frame.core.store import Store
 
-    query = ['WITH']
-    select = []
-    for i, array in enumerate(frame._blocks.iter_columns_arrays()):
-        exec(f'a{i} = array')
-        select.append(f't{i} AS (SELECT ROW_NUMBER() OVER() AS rownum, * FROM a{i})')
-    query.append(', '.join(select))
+class StoreDuckDB(Store):
 
-    select = ['SELECT']
-    for i, col in enumerate(frame.columns):
-        select.append(f't{i}.column0 AS {col},')
-    select.append('from t0')
-    query.extend(select)
+    @classmethod
+    def frame_to_connection(cls,
+            *,
+            frame: TFrameAny,
+            # label: str, # can be None
+            connection: 'DuckDBPyConnection',
+            include_index: bool,
+            include_columns: bool,
+            ) -> 'DuckDBPyConnection':
+        '''
+        Args:
+            label: string to be used as the table name.
+        '''
+        field_names, dtypes = cls.get_field_names_and_dtypes(
+                frame=frame,
+                include_index=include_index,
+                include_index_name=True,
+                include_columns=include_columns,
+                include_columns_name=False,
+                force_brackets=True # needed for having numbers as field names
+                )
 
-    r = range(frame.shape[1])
-    for i, j in zip(r[:-1], r[1:]):
-        query.append(f'join t{j} on t{i}.rownum = t{j}.rownum')
+        # frame._blocks.iter_columns_arrays()
+        label_arrays = zip(field_names,
+                cls.get_column_iterator(frame, include_index=include_index)
+                )
 
-    msg = ' '.join(query)
-    return connection.execute(msg)
+        query = ['WITH']
+        w = []
+        s = ['SELECT']
+
+        for i, (label, array) in enumerate(label_arrays):
+            exec(f'a{i} = array')
+            w.append(f't{i} AS (SELECT ROW_NUMBER() OVER() AS rownum, * FROM a{i})')
+            s.append(f't{i}.column0 AS {label},')
+
+        query.append(', '.join(w))
+        s.append('from t0')
+        query.extend(s)
+
+        r = range(frame.shape[1])
+        for i, j in zip(r[:-1], r[1:]):
+            query.append(f'join t{j} on t{i}.rownum = t{j}.rownum')
+
+        msg = ' '.join(query)
+        return connection.execute(msg)
+
+    @classmethod
+    def connection_to_frame(cls,
+            *,
+            connection: 'DuckDBPyConnection',
+            ) -> TFrameAny:
+        pass
