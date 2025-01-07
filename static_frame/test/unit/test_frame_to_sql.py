2bb64fc887fca797d665807f6d17371c2a83d30e
flexatone <ariza@flexatone.com>
2025-01-06 19:45:38 -0800

    added test

--- static_frame/test/unit/test_frame_to_sql.py
+++ static_frame/test/unit/test_frame_to_sql.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 import frame_fixtures as ff
+import pytest
 
 # from static_frame.core.frame import Frame
 from static_frame.test.test_case import temp_file
@@ -15,3 +16,16 @@ def test_frame_to_sql_a():
         f.to_sql(conn)
         post = list(conn.cursor().execute('select * from f1'))
         assert post == [(0, 'zjZQ', 'zaji', 'ztsv'), (1, 'zO5l', 'zJnC', 'zUvW')]
+
+
+
+
+def test_frame_to_sql_b():
+
+    with temp_file('.db') as fp:
+        conn = sqlite3.connect(fp)
+
+        f = ff.parse('s(2,3)|v(str)').relabel(columns=('a', 'b', 'c'))
+
+        with pytest.raises(RuntimeError):
+            f.to_sql(conn)
