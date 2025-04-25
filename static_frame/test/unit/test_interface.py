7034fe877b8093e734ca91c05a46ae66f654f75f
flexatone <ariza@flexatone.com>
2025-04-25 13:24:31 -0700

    format

--- static_frame/test/unit/test_interface.py
+++ static_frame/test/unit/test_interface.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import numpy as np
 
 from static_frame import FillValueAuto
-from static_frame.core.bus import Bus
 from static_frame.core.batch import Batch
+from static_frame.core.bus import Bus
 from static_frame.core.frame import Frame
 from static_frame.core.frame import FrameGO
 from static_frame.core.index import Index
@@ -16,13 +16,12 @@ from static_frame.core.interface import InterfaceGroup
 from static_frame.core.interface import InterfaceSummary
 from static_frame.core.interface import _get_signatures
 from static_frame.core.interface import valid_argument_types
+from static_frame.core.quilt import Quilt
 from static_frame.core.series import Series
 from static_frame.core.type_clinic import Require
 from static_frame.core.www import WWW
-from static_frame.test.test_case import TestCase
 from static_frame.core.yarn import Yarn
-from static_frame.core.quilt import Quilt
-
+from static_frame.test.test_case import TestCase
 
 
 class TestUnit(TestCase):
