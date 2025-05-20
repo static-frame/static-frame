55d649a431344db6661d2dd7ff470f4e418c2e60
flexatone <ariza@flexatone.com>
2025-05-19 18:24:30 -0700

    progress

--- doc/source/articles/nptyping.py
+++ doc/source/articles/nptyping.py
@@ -1,18 +1,22 @@
-# import numpy as np
+import numpy as np
 # import static_frame as sf
+import typing as tp
 
 
-# def process1(x: np.ndarray[tuple[int], np.dtype[np.int16]]): ...
-
-def process1(x: int): ...
-
-# a1 = np.empty(100, dtype=np.int16)
-# process1(a1) # mypy passes
-
-# a2 = np.empty(100, dtype=np.float32)
-# process1(a2) # mypy fails
+def process1(x: np.ndarray[tuple[int], np.dtype[np.integer]]): ...
 
 
-process1(None)
-print('done')
+a1 = np.empty(100, dtype=np.int16)
+process1(a1) # mypy passes
+
+a2 = np.empty(100, dtype=np.float32)
+process1(a2) # mypy fails
+# nptyping.py:12: error: Argument 1 to "process1" has incompatible type "ndarray[tuple[int], dtype[floating[_32Bit]]]"; expected "ndarray[tuple[int], dtype[integer[Any]]]"
+
+
+a3 = np.empty((100, 100, 100), dtype=np.int64)
+process1(a3) # mypy fails
+# nptyping.py:18: error: Argument 1 to "process1" has incompatible type "ndarray[tuple[int, int, int], dtype[signedinteger[_64Bit]]]"; expected "ndarray[tuple[int], dtype[integer[Any]]]"
+
+
 
