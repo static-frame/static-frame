66ae397b80608fc6a8cc9d38d95a37436fa322e6
Charles Burkland <burkland@rallc.com>
2025-06-18 10:48:35 -0700

    Fixes bug & adds test

--- static_frame/test/unit/test_index_hierarchy_set_utils.py
+++ static_frame/test/unit/test_index_hierarchy_set_utils.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import unittest
 from string import ascii_letters
 
+import numpy as np
+
 from static_frame.core.index_base import IndexBase
 from static_frame.core.index_hierarchy import IndexHierarchy
 from static_frame.core.index_hierarchy_set_utils import (
@@ -36,7 +38,7 @@ class TestUnit(TestCase):
             indices.append(ih.iloc[sl])
 
         # 1.51 s ± 63.4 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
-        expected = IndexBase.union(*indices).sort()
+        expected = IndexBase.union(*indices).sort()  # type: ignore
 
         # 17.4 ms ± 162 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
         actual = index_hierarchy_union(*indices).sort()
@@ -58,7 +60,7 @@ class TestUnit(TestCase):
             indices.append(ih.iloc[i:])
 
         # 4.44 s ± 66.8 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
-        expected = IndexBase.intersection(*indices).sort()
+        expected = IndexBase.intersection(*indices).sort()  # type: ignore
 
         # 219 ms ± 1.34 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
         actual = index_hierarchy_intersection(*indices).sort()
@@ -90,7 +92,7 @@ class TestUnit(TestCase):
             indices.append(ih.iloc[sl])
 
         # 46.5 ms ± 3.35 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
-        expected = IndexBase.difference(*indices).sort()
+        expected = IndexBase.difference(*indices).sort()  # type: ignore
 
         # 23.2 ms ± 564 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
         actual = index_hierarchy_difference(*indices).sort()
@@ -99,6 +101,28 @@ class TestUnit(TestCase):
             actual.equals(expected),
             msg=(expected.rename('expected'), actual.rename('actual')),
         )
+
+    def test_index_hierarchy_ops_with_object_mappings(self) -> None:
+        ih1 = IndexHierarchy.from_labels(np.arange(0, 1_300).reshape(130, 10))
+        ih2 = IndexHierarchy.from_labels(np.arange(1_300, 2_600).reshape(130, 10))
+
+        ih3 = ih1.intersection(ih2)
+        ih4 = ih1.union(ih2)
+        ih5 = ih1.difference(ih2)
+        ih6 = ih2.intersection(ih1)
+        ih7 = ih2.union(ih1)
+        ih8 = ih2.difference(ih1)
+
+        assert ih3.shape == (0, 10)
+        assert ih4.shape == (260, 10)
+        assert ih5.shape == (130, 10)
+        assert ih6.shape == (0, 10)
+        assert ih7.shape == (260, 10)
+        assert ih8.shape == (130, 10)
+
+        assert ih3.equals(ih6, compare_dtype=True, compare_name=True, compare_class=True)
+        assert ih4.equals(ih7, compare_dtype=True, compare_name=True, compare_class=True)
+        assert not ih5.equals(ih8)
 
 
 if __name__ == '__main__':
