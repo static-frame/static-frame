ddc3826cd6b9277b77efdb1d850c0f5f1a2cd1e0
flexatone <ariza@flexatone.com>
2025-06-26 08:22:49 -0700

    3.2.0 RC 1

--- static_frame/__init__.py
+++ static_frame/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 
 # We import the names "as" themselves here (and here only) to tell linting tools
 # that they are explicitly being exported here (and not just unused).
